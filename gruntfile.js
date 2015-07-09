b09367c384b982a210489da60a6efe6a6234635a
James Ferguson <james.ferguson@skyscanner.net>
2015-07-09 16:36:26 +0100

    - added grunt file to watch folder and concat stylus files into one master file

--- gruntfile.js
+++ gruntfile.js
@@ -2,13 +2,21 @@ module.exports = function (grunt) {
     grunt.initConfig({
         concat: {
             styles: {
-                src: ['stylus/*.scss'],
-                dest: 'stylus/generated/common-styles.scss'
+                src: ['stylus/*.styl'],
+                dest: 'common-styles.styl'
+            }
+        },
+        watch: {
+            styles: {
+                files: ['stylus/*.styl'],
+                tasks: ['concat']
             }
         }
     });
 
 
     grunt.loadNpmTasks('grunt-contrib-concat');
-    grunt.registerTask('build', ['concat']);
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    grunt.registerTask('default', ['concat', 'watch']);
 };
