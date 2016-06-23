1f3c76bd85b8990a9452fb71cb2dec033f7181f9
Matthew Davidson <matthew.davidson@skyscanner.net>
2016-06-23 18:17:21 +0100

    BPK-17 - Fixed some formatting on the base stylesheet page

--- docs/pages/BaseStylesheetPage/BaseStylesheetPage.jsx
+++ docs/pages/BaseStylesheetPage/BaseStylesheetPage.jsx
@@ -15,8 +15,7 @@ const BaseStylesheetPage = () => (
         <li>A minimal, relatively un-opinionated base stylesheet</li>
         <li>
           For improved cross-browser rendering, we use <a href='http://necolas.github.io/normalize.css/'
-            target='__blank'>normalize.css</a> to correct small
-          inconsistencies across browsers and devices.
+            target='__blank'>normalize.css</a> to correct small inconsistencies across browsers and devices.
         </li>
         <li>
           The <code>box-sizing</code> is globally set on every element—including <code>*:before</code> and
@@ -24,8 +23,8 @@ const BaseStylesheetPage = () => (
           exceeded due to padding or border.
         </li>
         <li>
-          Utility classes based on the utils mixins above, including: `.hidden`, `.visuallyhidden`,
-          `.visuallyhidden.focusable`, `.invisible` and `.clearfix`
+          Utility classes based on the utils mixins above, including: <code>.hidden</code>, <code>.visuallyhidden</code>,
+          <code>.visuallyhidden.focusable</code>, <code>.invisible</code> and <code>.clearfix</code>.
         </li>
         <li>That's it!</li>
       </ul>
