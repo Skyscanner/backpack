fb469fc995b3c1f7f0035d513ec73b5e7c45a494
Matthew Davidson <matthew.davidson@skyscanner.net>
2016-06-24 11:45:04 +0100

    BPK-17 - Updated some notes about base stylesheets - still to add proper links to the blog post and Jira card

--- docs/pages/BaseStylesheetPage/BaseStylesheetPage.jsx
+++ docs/pages/BaseStylesheetPage/BaseStylesheetPage.jsx
@@ -5,17 +5,37 @@ const BaseStylesheetPage = () => (
   <div>
     <Helmet title='Base Stylesheet' />
     <section>
+      <p>
+        <strong>
+          Note: The scaffolding still uses an different, older base stylesheet. Please refer to this <a href='#'>blog
+          post</a> for more info and this <a href='#'>JIRA card</a> for updates.
+        </strong>
+      </p>
       <h2>Example Usage</h2>
+      <p>
+        Consumer's of the website scaffolding should expect this base stylesheet to be on the page already.
+        If you need to emulate this base in your component's local test harness, the compiled css is available for you
+        to import in your test harness:
+      </p>
+      <pre>
+        <code>
+          {'<link rel="stylesheet" href="/node_modules/living-styles/base.css">'}
+        </code>
+      </pre>
+      <p>
+        Alternatively you could import using sass:
+      </p>
       <pre>
         <code>
           {'@import "~/living-styles/base";'}
         </code>
       </pre>
+      <h3>What's in it?</h3>
       <ul>
         <li>A minimal, relatively un-opinionated base stylesheet</li>
         <li>
-          For improved cross-browser rendering, we use <a href='http://necolas.github.io/normalize.css/'
-            target='__blank'>normalize.css</a> to correct small inconsistencies across browsers and devices.
+          For improved cross-browser rendering, we use <a href='http://necolas.github.io/normalize.css/' target='__blank'>normalize.css</a> to correct small
+          inconsistencies across browsers and devices.
         </li>
         <li>
           The <code>box-sizing</code> is globally set on every element—including <code>*:before</code> and
