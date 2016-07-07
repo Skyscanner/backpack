240da11211e48c605cf2829de1ed1949625ed3b3
Matthew Davidson <matthew.davidson@skyscanner.net>
2016-07-07 11:58:20 +0100

    Added sassdoc links

--- docs/components/BpkCode/BpkCode.jsx
+++ docs/components/BpkCode/BpkCode.jsx
@@ -1,65 +1,31 @@
-import Prism from 'prismjs'
-import Beautify from 'js-beautify'
+import beautify from 'js-beautify'
 import React, { PropTypes } from 'react'
 import CssModules from 'react-css-modules'
 import ReactDOMServer from 'react-dom/server'
 
 import styles from './bpk-code.scss'
 
-const syntaxHighlightingMap = {
-  html: {
-    beautifier: 'html',
-    highlighter: Prism.languages.markup
-  },
-  css: {
-    beautifier: 'css',
-    highlighter: Prism.languages.css
-  },
-  js: {
-    beautifier: 'js',
-    highlighter: Prism.languages.javascript
+const childrenToString = (children) => {
+  return React.Children.map(children, (child) => React.isValidElement(child)
+    ? ReactDOMServer.renderToStaticMarkup(child)
+    : `${child}`
+  ).join('')
+}
+
+const prettyPrint = (code, syntax) => beautify[ syntax ](code, { indent_size: 2 })
+
+const BpkCode = ({ children, syntax, inline }) => {
+  const codeString = childrenToString(children)
+
+  if (inline) {
+    return <code styleName='bpk-code__code bpk-code__code--inline'>{codeString}</code>
   }
-}
 
-const renderToStaticMarkup = (element) => {
-  return React.isValidElement(element)
-    ? ReactDOMServer.renderToStaticMarkup(element)
-    : `${element}`
-}
-
-const renderChildrenToStaticMarkup = (children) => {
-  return React.Children.map(children, (child) => renderToStaticMarkup(child)).join('')
-}
-
-const prettyPrint = (code, syntax) => {
-  const { beautifier } = syntaxHighlightingMap[ syntax ]
-
-  return syntax ? Beautify[ beautifier ](code, { indent_size: 2 }) : code
-}
-
-const highlight = (code, syntax) => {
-  const { highlighter } = syntaxHighlightingMap[ syntax ]
-
-  return syntax ? Prism.highlight(code, highlighter) : code
-}
-
-const BpkCode = (props) => {
-  const styleName = `language-${props.syntax}`
-  const staticMarkup = renderChildrenToStaticMarkup(props.children)
-  const prettyCode = prettyPrint(staticMarkup, props.syntax)
-  const innerHTML = { __html: highlight(prettyCode, props.syntax) }
-
-  if (props.inline) {
-    return (
-      <code styleName={`${styleName} ${styleName}--inline`}>
-        <span dangerouslySetInnerHTML={innerHTML}></span>
-      </code>
-    )
-  }
+  const prettyCodeString = prettyPrint(codeString, syntax)
 
   return (
-    <pre styleName={`${styleName} ${styleName}--block`}>
-      <code styleName={styleName} dangerouslySetInnerHTML={innerHTML}></code>
+    <pre styleName='bpk-code__pre'>
+      <code styleName='bpk-code__code bpk-code__code--block'>{prettyCodeString}</code>
     </pre>
   )
 }
