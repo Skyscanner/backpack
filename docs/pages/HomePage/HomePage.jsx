a2b631e92d1d520b35a501176507b075cb63e1f5
Matthew Davidson <matthew.davidson@skyscanner.net>
2016-07-19 16:11:45 +0100

    Upgrading backpack-tokens to v7.0.0

--- docs/pages/HomePage/HomePage.jsx
+++ docs/pages/HomePage/HomePage.jsx
@@ -10,53 +10,60 @@ import BpkHeading from 'bpk-component-heading'
 import BpkParagraph from 'bpk-component-paragraph'
 import BpkLink from './../../components/BpkLink'
 
-const anchors = {
-  gettingStarted: 'home-page-getting-started',
-  designTokens: 'home-page-design-tokens',
-  sketchFiles: 'home-page-sketch-files',
-  stylesheets: 'home-page-stylesheets',
-  sassMixins: 'home-page-sass-mixins',
-  reactComponents: 'home-page-react-components'
-}
-
 const HomePage = () => (
   <section>
     <Helmet title='Backpack' />
     <div styleName='bpkdocs-home-page__hero'>
       <div styleName='bpkdocs-home-page__hero-container'>
-        <BpkHeading level='h1'>Weclome to Backpack</BpkHeading>
-        <BpkHeading level='h2'>Backpack is a suite of tools to enable you to design and build products which look and feel consistent with the Skyscanner product family.</BpkHeading>
-        <BpkButton large href={`#${anchors.gettingStarted}`}>Get started</BpkButton>
+        <BpkHeading level='h1'>Backpack</BpkHeading>
+        <BpkHeading level='h2'>
+          A suite of tools to enable you to design and build products which look and
+          feel consistent with the Skyscanner product family.
+        </BpkHeading>
+        <BpkButton large>Get started</BpkButton>
       </div>
     </div>
     <div styleName='bpkdocs-home-page__content-container'>
-      <BpkHeading level='h3'>We’ve built upon Atomic Design principals to bring a scalable platform to design, build and share components across all of Skyscanner’s product areas.</BpkHeading>
+      <BpkHeading level='h3'>
+        We’ve built upon Atomic Design principals to bring a scalable platform to design, build and share components
+        across all of Skyscanner’s product areas.
+      </BpkHeading>
       <section styleName='bpkdocs-home-page__columns'>
         <article styleName='bpkdocs-home-page__column'>
           <BpkHeading level='h3'>Bonds</BpkHeading>
           <BpkParagraph>
-            Bonds help set the foundation for all elements and helps connect them together. They’re abstract by nature and cover things like colour, typography and even more invisible aspects of an interface such as animations and spacing.<br />
+            Bonds help set the foundation for all elements and helps connect them together. They’re abstract by nature
+            and cover things like colour, typography and even more invisible aspects of an interface such as animations
+            and spacing.
+            <br />
             <BpkLink to={routes.BONDS}>Read more</BpkLink>
           </BpkParagraph>
         </article>
         <article styleName='bpkdocs-home-page__column'>
           <BpkHeading level='h3'>Atoms</BpkHeading>
           <BpkParagraph>
-            Atoms are the basic building blocks of matter. Applied to web interfaces, atoms are our HTML tags, such as a form label, an input or a button. They have distinct properties and can't be broken down further without losing their meaning.<br />
+            Atoms are the basic building blocks of matter. Applied to web interfaces, atoms are our HTML tags, such as a
+            form label, an input or a button. They have distinct properties and can't be broken down further without
+            losing their meaning.
+            <br />
             <BpkLink to={routes.ATOMS}>Read more</BpkLink>
           </BpkParagraph>
         </article>
         <article styleName='bpkdocs-home-page__column'>
           <BpkHeading level='h3'>Molecules</BpkHeading>
           <BpkParagraph>
-            Molecules are groups of two or more atoms held together by chemical bonds. These combinations of atoms take on their own unique properties, and become more tangible and operational than atoms.<br />
+            Molecules are two or more atoms held together by chemical bonds. These combinations of atoms take on their
+            own unique properties, and become more tangible and operational than atoms.
+            <br />
             <BpkLink to={routes.MOLECULES}>Read more</BpkLink>
           </BpkParagraph>
         </article>
         <article styleName='bpkdocs-home-page__column'>
           <BpkHeading level='h3'>Organisms</BpkHeading>
           <BpkParagraph>
-            Organisms are groups of molecules joined together to form a relatively complex, distinct section of an interface.<br />
+            Organisms are groups of molecules joined together to form a relatively complex, distinct section of an
+            interface.
+            <br />
             <BpkLink to={routes.ORGANISMS}>Read more</BpkLink>
           </BpkParagraph>
         </article>
