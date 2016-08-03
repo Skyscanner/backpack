d003c04b751086112a040cd1eebb1655f7cb618d
James Ferguson <james.ferguson@skyscanner.net>
2016-08-03 19:56:44 +0100

    Adding Favicons

--- packages/bpk-docs/src/template.js
+++ packages/bpk-docs/src/template.js
@@ -12,6 +12,12 @@ export default ({ head = {}, html = '', assets = {} }) => {
   <meta name="robots" content="noindex">
   ${head.title.toString()}
   <link rel="stylesheet" href="/${assets.docs.css}">
+  <link rel="apple-touch-icon" sizes="180x180" href="../../img/favicons/apple-touch-icon.png">
+  <link rel="icon" type="image/png" href="../../img/favicons/favicon-32x32.png" sizes="32x32">
+  <link rel="icon" type="image/png" href="../../img/favicons/favicon-16x16.png" sizes="16x16">
+  <link rel="manifest" href="../../img/favicons/manifest.json">
+  <link rel="mask-icon" href="../../img/favicons/safari-pinned-tab.svg" color="#fd5250">
+  <meta name="theme-color" content="#40c4df">
 </head>
 
 <body>
