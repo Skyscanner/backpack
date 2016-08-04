1c0f7d6933b5fbbaba421d031a796c7092a7bb1f
James Ferguson <james.ferguson@skyscanner.net>
2016-08-04 13:24:06 +0100

    BPK-134: Added navigation

--- packages/bpk-docs/src/layouts/DocsLayout/DocsLayout.js
+++ packages/bpk-docs/src/layouts/DocsLayout/DocsLayout.js
@@ -10,7 +10,9 @@ const links = [
     links: [
       { route: routes.BASE_STYLESHEET, children: 'Base stylesheet' },
       { route: routes.COLORS, children: 'Colors' },
-      { route: routes.FONTS_AND_SPACING, children: 'Fonts and spacing' }
+      { route: routes.FONTS_AND_SPACING, children: 'Baseline grid & type ramp' },
+      { route: null, children: 'Layout' },
+      { route: null, children: 'Motion' }
     ]
   },
   {
@@ -20,7 +22,45 @@ const links = [
       { route: routes.BUTTONS, children: 'Buttons' },
       { route: routes.ICONS, children: 'Icons' },
       { route: routes.SPINNERS, children: 'Spinners' },
-      { route: routes.LOGOS, children: 'Logos' }
+      { route: routes.LOGOS, children: 'Logos' },
+      { route: null, children: 'Forms' },
+      { route: null, children: 'Cards' }
+    ]
+  },
+  {
+    category: 'Molecules',
+    links: [
+      { route: null, children: 'Banner alerts' },
+      { route: null, children: 'Modal' },
+      { route: null, children: 'Popover' },
+      { route: null, children: 'Tooltip' },
+      { route: null, children: 'Pagination' },
+      { route: null, children: 'Calendar' },
+      { route: null, children: 'Numerical stepper' },
+      { route: null, children: 'Tabs' },
+      { route: null, children: 'Primary navigation' },
+      { route: null, children: 'Secondary navigation' },
+      { route: null, children: 'Utility navigation' },
+      { route: null, children: 'Hamburger navigation' },
+      { route: null, children: 'Numerical rating' },
+      { route: null, children: 'Star rating' }
+    ]
+  },
+  {
+    category: 'Organisms',
+    links: [
+      { route: null, children: 'Flight itinerary' },
+      { route: null, children: 'Autosuggest' },
+      { route: null, children: 'Date picker' },
+      { route: null, children: 'Filters' },
+      { route: null, children: 'Header' },
+      { route: null, children: 'Footer' }
+    ]
+  },
+  {
+    category: 'Systems',
+    links: [
+      { route: null, children: 'Search controls' },
     ]
   }
 ]
