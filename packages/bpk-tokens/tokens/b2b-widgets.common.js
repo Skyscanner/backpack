f3144c65217ead4aefe8807d874acde184c9f2e9
David Futcher <david.futcher@skyscanner.net>
2016-08-05 16:26:48 +0100

    BW-245 Add majority of b2b-widgets tokens

--- packages/bpk-tokens/tokens/b2b-widgets.common.js
+++ packages/bpk-tokens/tokens/b2b-widgets.common.js
@@ -1,5 +1,18 @@
 module.exports = {
-  buttonBackgroundColor: "rgb(255, 0, 0)",
+  buttonPaddingY: "18px",
+  buttonPaddingX: "20px",
+  buttonBackgroundColor: "rgb(255, 255, 255)",
   buttonBackgroundImage: "none",
-  buttonBorderRadius: "none"
+  buttonColor: "rgb(0, 105, 130)",
+  buttonBorderRadius: "none",
+  buttonHoverBackgroundColor: "rgb(0, 105, 130)",
+  buttonHoverColor: "rgb(255, 255, 255)",
+  buttonDisabledBackgroundColor: "rgb(0, 105, 130)",
+  buttonDisabledColor: "rgb(255, 255, 255)",
+  buttonSelectedColor: "rgb(255, 255, 255)",
+  buttonSelectedBackgroundColor: "rgb(0, 105, 130)",
+  buttonSelectedBackgroundImage: "none",
+  buttonSelectedHoverColor: "rgb(255, 255, 255)",
+  buttonSelectedHoverBackgroundColor: "rgb(0, 105, 130)",
+  buttonActiveBackgroundColor: "rgb(0, 105, 130)"
 };
