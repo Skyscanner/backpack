f5a56520b4e733ddeabec68688a79763661dc64a
James Ferguson <james.ferguson@skyscanner.net>
2016-08-09 08:33:55 +0100

    BPK-127: Adding docs for logos

--- packages/bpk-docs/src/pages/LogosPage/LogosPage.js
+++ packages/bpk-docs/src/pages/LogosPage/LogosPage.js
@@ -24,8 +24,15 @@ const LogosPage = () => (
     <BpkContentContainer>
       <BpkHeading level='h1'>Logos</BpkHeading>
       <BpkParagraph>
-        Logos are available in five variants - cloud, inline, stacked Tianxun and Tianxun-stacked.
+        We have a few different versions of our logo which can be used in different situations. In general, all logos
+        should be used in white, reversed out of a colored background. When it’s not possible to reverse the logo out
+        of a color, the preferred usage is using blue-500 or gray-500. No other colors should be used.
       </BpkParagraph>
+      <BpkParagraph>
+        As with using any logo, please ensure there’s sufficient clear space surrounding it and consider color,
+        contrast, and legibility to ensure it’s shown at its best.
+      </BpkParagraph>
+      <BpkHeading level='h2'>Examples</BpkHeading>
       <BpkList>
         <BpkListItem>
           <BpkLink href={`#${anchors.cloud}`}>Cloud</BpkLink>
@@ -43,26 +50,52 @@ const LogosPage = () => (
           <BpkLink href={`#${anchors.tianxunStacked}`}>Tianxun stacked</BpkLink>
         </BpkListItem>
       </BpkList>
-      <BpkHeading id={anchors.cloud} level='h2'>Cloud</BpkHeading>
-      <PresentationBlock>
-        <BpkLogo logo='cloud' />
-      </PresentationBlock>
-      <BpkHeading id={anchors.inline} level='h2'>Inline</BpkHeading>
+      <BpkHeading id={anchors.inline} level='h3'>Inline</BpkHeading>
+      <BpkParagraph>
+        This is the preferred usage of the logo.
+      </BpkParagraph>
       <PresentationBlock>
         <BpkLogo logo='inline' />
       </PresentationBlock>
-      <BpkHeading id={anchors.stacked} level='h2'>Stacked</BpkHeading>
+      <BpkHeading id={anchors.stacked} level='h3'>Stacked</BpkHeading>
+      <BpkParagraph>
+        This version should only be used when required to be used at small sizes, usually confined to a square e.g. a
+        social media profile.
+      </BpkParagraph>
       <PresentationBlock>
         <BpkLogo logo='stacked' />
       </PresentationBlock>
+      <BpkHeading id={anchors.cloud} level='h3'>Cloud only</BpkHeading>
+      <BpkParagraph>
+        This version should be used sparingly for things such as App icons and favicons.
+      </BpkParagraph>
+      <PresentationBlock>
+        <BpkLogo logo='cloud' />
+      </PresentationBlock>
       <BpkHeading id={anchors.tianxun} level='h2'>Tianxun</BpkHeading>
+      <BpkParagraph>
+        This is the preferred usage for Skyscanner in China.
+      </BpkParagraph>
       <PresentationBlock>
         <BpkLogo logo='tianxun' />
       </PresentationBlock>
       <BpkHeading id={anchors.tianxunStacked} level='h2'>Tianxun stacked</BpkHeading>
+      <BpkParagraph>
+        Similar to the stacked version of the Skyscanner logo, this version should only be used at small sizes when
+        constrained to a square e.g. social media profile.
+      </BpkParagraph>
       <PresentationBlock>
         <BpkLogo logo='tianxun-stacked' />
       </PresentationBlock>
+      <BpkHeading level='h2'>Localised Product Logos</BpkHeading>
+      <BpkParagraph>
+        We also maintain specific logos for our product verticals,  for example:  ‘Skyscanner Flights’,
+        ‘Skyscanner Hotels’ etc.. These are not widely used on our product and as such are not maintained in Backpack.
+        Please contact  the <BpkLink href='mailto: creative@skyscanner.net'>creative team </BpkLink> if you require
+        these.
+      </BpkParagraph>
+      <BpkHeading level='h2'>Usage</BpkHeading>
+      <BpkParagraph>Matt to write</BpkParagraph>
     </BpkContentContainer>
   </section>
 )
