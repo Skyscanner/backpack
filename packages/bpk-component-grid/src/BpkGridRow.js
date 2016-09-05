0aa20445894a76008cabc26c0ad1d05f33afe451
Matthew Davidson <matthew.davidson@skyscanner.net>
2016-09-05 18:01:31 +0100

    BPK-76 - Columns now have top and bottom padding - rows negate that padding on the first and last rows

--- packages/bpk-component-grid/src/BpkGridRow.js
+++ packages/bpk-component-grid/src/BpkGridRow.js
@@ -2,24 +2,13 @@ import React, { PropTypes } from 'react'
 
 import './bpk-grid.scss'
 
-const BpkGridRow = (props) => {
-  const classNames = [ 'bpk-grid__row' ]
-
-  props.debug ? classNames.push('bpk-grid__row--debug') : null
-
-  return <div className={classNames.join(' ')}>{props.children}</div>
-}
+const BpkGridRow = (props) => <div className='bpk-grid__row'>{props.children}</div>
 
 BpkGridRow.propTypes = {
   children: PropTypes.oneOfType([
     PropTypes.arrayOf(PropTypes.node),
     PropTypes.node
-  ]).isRequired,
-  debug: PropTypes.bool
-}
-
-BpkGridRow.defaultProps = {
-  debug: false
+  ]).isRequired
 }
 
 export default BpkGridRow
