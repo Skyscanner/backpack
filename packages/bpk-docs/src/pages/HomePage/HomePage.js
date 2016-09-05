227bc4c0fc2adca514740c4c26f2e4c8f5ee450d
Matthew Davidson <matthew.davidson@skyscanner.net>
2016-09-05 11:42:25 +0100

    Consuming bpk-component-grid in homepage and downloads page

--- packages/bpk-docs/src/pages/HomePage/HomePage.js
+++ packages/bpk-docs/src/pages/HomePage/HomePage.js
@@ -7,6 +7,7 @@ import BpkButton from 'bpk-component-button'
 import BpkHeading from 'bpk-component-heading'
 import BpkParagraph from 'bpk-component-paragraph'
 import TOKENS from 'bpk-tokens/tokens/base.common'
+import { BpkGridContainer, BpkGridRow, BpkGridColumn } from 'bpk-component-grid'
 
 import './home-page.scss'
 import * as ROUTES from './../../constants/routes'
@@ -28,33 +29,38 @@ class HomePage extends React.Component {
       <section>
         <Helmet title='Backpack' />
         <div className='bpkdocs-home-page__hero'>
-          <div className='bpkdocs-home-page__hero-container'>
-            <BpkHeading level='h1'>Backpack</BpkHeading>
-            <BpkHeading level='h2'>Backpack is a collection of design resources, reusable components and guidelines for
-              creating Skyscanner products.
-            </BpkHeading>
-            <BpkButton large href={ROUTES.GETTING_STARTED} onClick={this.onGettingStartedClick}>
-              Get started <BpkIcon icon='long-arrow-right-alt' color={TOKENS.colorWhite} large alignToButton />
-            </BpkButton>
-          </div>
+          <BpkGridContainer>
+            <BpkGridRow>
+              <BpkGridColumn width={12}>
+                <BpkHeading level='h1'>Backpack</BpkHeading>
+                <BpkHeading level='h2'>
+                  Backpack is a collection of design resources, reusable components and guidelines for creating
+                  Skyscanner products.
+                </BpkHeading>
+                <BpkButton large href={ROUTES.GETTING_STARTED} onClick={this.onGettingStartedClick}>
+                  Get started <BpkIcon icon='long-arrow-right-alt' color={TOKENS.colorWhite} large alignToButton />
+                </BpkButton>
+              </BpkGridColumn>
+            </BpkGridRow>
+          </BpkGridContainer>
         </div>
-        <div className='bpkdocs-home-page__content-container'>
-          <section className='bpkdocs-home-page__columns'>
-            <article className='bpkdocs-home-page__column'>
+        <BpkGridContainer>
+          <BpkGridRow>
+            <BpkGridColumn width={6} tabletWidth={12}>
               <BpkHeading level='h3'>Mission</BpkHeading>
               <BpkParagraph>To bring Design and Engineering teams together to help drive product consistency
                 at scale and reduce waste by offering a platform by which to share components easily.
               </BpkParagraph>
-            </article>
-            <article className='bpkdocs-home-page__column'>
+            </BpkGridColumn>
+            <BpkGridColumn width={6} tabletWidth={12}>
               <BpkHeading level='h3'>About Backpack</BpkHeading>
               <BpkParagraph>
                 Backpack is the foundation for all Skyscanner products and is maintained by the Backpack Design System
                 Squad. It builds on Atomic Design principals to help visualise how Skyscanner's products are assembled.
               </BpkParagraph>
-            </article>
-          </section>
-        </div>
+            </BpkGridColumn>
+          </BpkGridRow>
+        </BpkGridContainer>
       </section>
     )
   }
