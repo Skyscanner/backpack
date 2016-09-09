97787ff33b33d18a97f7cabfa0a4412f4d6da631
David Futcher <david.futcher@skyscanner.net>
2016-09-09 16:21:02 +0100

    Upgrade react-autosuggest version to v6.x.x, expose all react-autosuggest properties and add PropTypes for BpkAutosuggest (as interface does not match react-autosuggest exactly)

--- packages/bpk-component-autosuggest/src/BpkAutosuggest.js
+++ packages/bpk-component-autosuggest/src/BpkAutosuggest.js
@@ -1,18 +1,18 @@
-import React from 'react'
+import React, { PropTypes } from 'react'
 import Autosuggest from 'react-autosuggest'
 
 import style from './bpk-autosuggest.scss'
 
 const defaultTheme = {
-  container:            'bpk-autosuggest__container',
-  containerOpen:        'bpk-autosuggest__container--open',
-  input:                'bpk-autosuggest__input',
+  container: 'bpk-autosuggest__container',
+  containerOpen: 'bpk-autosuggest__container--open',
+  input: 'bpk-autosuggest__input',
   suggestionsContainer: 'bpk-autosuggest__suggestions-container',
-  suggestionsList:      'bpk-autosuggest__suggestions-list',
-  suggestion:           'bpk-autosuggest__suggestion',
-  suggestionFocused:    'bpk-autosuggest__suggestion--focused',
-  sectionContainer:     'bpk-autosuggest__section-container',
-  sectionTitle:         'bpk-autosuggest__section-title'
+  suggestionsList: 'bpk-autosuggest__suggestions-list',
+  suggestion: 'bpk-autosuggest__suggestion',
+  suggestionFocused: 'bpk-autosuggest__suggestion--focused',
+  sectionContainer: 'bpk-autosuggest__section-container',
+  sectionTitle: 'bpk-autosuggest__section-title'
 }
 
 const BpkAutosuggest = (props) => {
@@ -24,14 +24,14 @@ const BpkAutosuggest = (props) => {
   return (
     <Autosuggest
       suggestions={props.suggestions}
-      onSuggestionsUpdateRequested={props.onSuggestionsUpdateRequested}
+      onSuggestionsFetchRequested={props.onSuggestionsFetchRequested}
       onSuggestionsClearRequested={props.onSuggestionsClearRequested}
       getSuggestionValue={props.getSuggestionValue}
       renderSuggestion={props.renderSuggestion}
       onSuggestionSelected={props.onSuggestionSelected}
       shouldRenderSuggestions={props.shouldRenderSuggestions}
       alwaysRenderSuggestions={props.alwaysRenderSuggestions}
-      focusFirstSuggstion={props.focusFirstSuggstion}
+      focusFirstSuggestion={props.focusFirstSuggestion}
       focusInputOnSuggestionClick={props.focusInputOnSuggestionClick}
       multiSection={props.multiSection}
       renderSectionTitle={props.renderSectionTitle}
@@ -47,6 +47,27 @@ const BpkAutosuggest = (props) => {
 BpkAutosuggest.defaultProps = {
   getSuggestionValue: (suggestion) => suggestion,
   theme: defaultTheme
+}
+
+BpkAutosuggest.propTypes = {
+  value: PropTypes.any.isRequired,
+  suggestions: PropTypes.array.isRequired,
+  onChange: PropTypes.func.isRequired,
+  onSuggestionsFetchRequested: PropTypes.func.isRequired,
+  onSuggestionsClearRequested: PropTypes.func.isRequired,
+  getSuggestionValue: PropTypes.func,
+  renderSuggestion: PropTypes.func.isRequired,
+  onSuggestionSelected: PropTypes.func,
+  shouldRenderSuggestions: PropTypes.bool,
+  alwaysRenderSuggestions: PropTypes.bool,
+  focusFirstSuggestion: PropTypes.bool,
+  focusInputOnSuggestionClick: PropTypes.bool,
+  multiSection: PropTypes.bool,
+  renderSectionTitle: PropTypes.func,
+  getSectionSuggestions: PropTypes.func,
+  renderSuggestionsContainer: PropTypes.func,
+  id: PropTypes.string,
+  theme: PropTypes.object
 }
 
 export default BpkAutosuggest
