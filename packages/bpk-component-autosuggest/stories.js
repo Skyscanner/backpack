a9cf844d28cb4ecb2450ad1ff531ac1073704cb6
David Futcher <david.futcher@skyscanner.net>
2016-09-09 16:22:10 +0100

    Working BpkAutosuggest example in stories

--- packages/bpk-component-autosuggest/stories.js
+++ packages/bpk-component-autosuggest/stories.js
@@ -3,25 +3,73 @@ import { storiesOf } from '@kadira/storybook'
 
 import BpkAutosuggest from './index'
 
+function getSuggestions () {
+  return [
+    {
+      display: 'London Heathrow',
+      code: 'LHR'
+    },
+    {
+      display: 'Edinburgh',
+      code: 'EDI'
+    },
+    {
+      display: 'Glasgow',
+      code: 'GLA'
+    }
+  ]
+}
+
 class AutosuggestExample extends React.Component {
 
-    render() {
-        const suggestions = [
-            'Example',
-            'Autosuggest',
-            'Suggestions'
-        ]
+  constructor () {
+    super()
 
-        return <BpkAutosuggest suggestions={suggestions}
-                               onSuggestionsUpdateRequested={() => {}}
-                               renderSuggestion={this.renderSuggestion}
-                               value={'Edinburgh'}
-                               onChange={() => {}} />
+    this.state = {
+      value: '',
+      suggestions: []
     }
+  }
 
-    renderSuggestion(suggestion) {
-        return (<span>{suggestion}</span>);
-    }
+  onChange (event, { newValue }) {
+    this.setState({
+      value: newValue
+    })
+  }
+
+  onSuggestionsClearRequested () {
+    this.setState({
+      suggestions: []
+    })
+  }
+
+  onSuggestionsFetchRequested () {
+    this.setState({
+      suggestions: getSuggestions()
+    })
+  }
+
+  getSuggestionValue (suggestion) {
+    return suggestion.display
+  }
+
+  renderSuggestion (suggestion) {
+    return <span>{suggestion.display} ({suggestion.code})</span>
+  }
+
+  render () {
+    const { value, suggestions } = this.state
+
+    return <BpkAutosuggest
+      suggestions={suggestions}
+      value={value}
+      onSuggestionsFetchRequested={this.onSuggestionsFetchRequested.bind(this)}
+      onSuggestionsClearRequested={this.onSuggestionsClearRequested.bind(this)}
+      getSuggestionValue={this.getSuggestionValue.bind(this)}
+      renderSuggestion={this.renderSuggestion.bind(this)}
+      onChange={this.onChange.bind(this)}
+           />
+  }
 
 }
 
