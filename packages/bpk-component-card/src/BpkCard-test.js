600766663d8b134690ad5ca043c902b71550f2c2
James Ferguson <james.ferguson@skyscanner.net>
2016-09-26 13:08:08 +0100

    BPK-66: Adding docs for cards

--- packages/bpk-component-card/src/BpkCard-test.js
+++ packages/bpk-component-card/src/BpkCard-test.js
@@ -32,15 +32,5 @@ describe('BpkCard', () => {
     ).toJSON()
     expect(tree).toMatchSnapshot()
   })
-
-  it('should render correctly with "focused" attribute', () => {
-    const tree = renderer.create(
-      <BpkCard href='//www.skyscanner.net' focused>
-        Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum
-        sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
-      </BpkCard>
-    ).toJSON()
-    expect(tree).toMatchSnapshot()
-  })
 })
 
