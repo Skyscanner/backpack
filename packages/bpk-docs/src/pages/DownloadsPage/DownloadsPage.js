f66c037b362f6e9dbe54204933649d576b3429b6
James Ferguson <james.ferguson@skyscanner.net>
2016-10-10 13:38:14 +0100

    BPK-250: Added link to logo files on Dropbox.

--- packages/bpk-docs/src/pages/DownloadsPage/DownloadsPage.js
+++ packages/bpk-docs/src/pages/DownloadsPage/DownloadsPage.js
@@ -4,9 +4,9 @@ import Helmet from 'react-helmet'
 import BpkLink from 'bpk-component-link'
 import BpkHeading from 'bpk-component-heading'
 import BpkParagraph from 'bpk-component-paragraph'
-import { BpkList, BpkListItem } from 'bpk-component-list'
+import {BpkList, BpkListItem} from 'bpk-component-list'
 import BpkContentContainer from 'bpk-component-content-container'
-import { BpkGridContainer, BpkGridRow, BpkGridColumn } from 'bpk-component-grid'
+import {BpkGridContainer, BpkGridRow, BpkGridColumn} from 'bpk-component-grid'
 
 const smallIconsZip = require('!!file?name=[name].[hash].zip!zip-it!./../../../../bpk-svgs/src/icons/sm/icons-sm')
 const largeIconsZip = require('!!file?name=[name].[hash].zip!zip-it!./../../../../bpk-svgs/src/icons/lg/icons-lg')
@@ -44,6 +44,19 @@ const DownloadsPage = () => (
             </BpkListItem>
             <BpkListItem>
               <BpkLink href={`/${largeIconsZip}`}>Large icons</BpkLink>
+            </BpkListItem>
+          </BpkList>
+          <BpkHeading level='h2'>Logos</BpkHeading>
+          <BpkList>
+            <BpkListItem>
+              <BpkLink href='https://www.dropbox.com/sh/spolkpatjaua6xi/AADon-XjEae9pRmLazfo3-R7a?dl=0' blank>
+                App icons
+              </BpkLink>
+            </BpkListItem>
+            <BpkListItem>
+              <BpkLink href='https://www.dropbox.com/sh/yvj8s9m030t0fur/AABDX6BTDsuxSdyqe2_twW-Ra?dl=0' blank>
+                Logos
+              </BpkLink>
             </BpkListItem>
           </BpkList>
           <BpkHeading level='h2'>Sketch</BpkHeading>
