f1d74643e35198edf73e7fa7876d3efeab743107
Kartikey Tewari <kartikey.tewari@skyscanner.net>
2017-02-02 18:49:22 +0800

    Sort icon lg and sm added.

---
+++ packages/bpk-svgs/dist/js/icons/sm/sort.js
@@ -0,0 +1,7 @@
+import React from "react";
+export default class SVG extends React.Component {
+  render() {
+    return <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 18 18" {...this.props}><path d="M4.15 12l4.849 4 4.849-4c.334-.357.083-1-.418-1H4.65c-.585 0-.836.643-.501 1z" /></svg>;
+  }
+
+}
