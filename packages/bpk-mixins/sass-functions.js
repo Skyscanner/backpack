c970f420dcf6a88e07e181d44b8f30f09669b6dd
Tim von Oldenburg <tim.vonoldenburg@skyscanner.net>
2017-03-17 11:47:38 +0000

    Convert sass-functions.js to commonjs and reenable linting

--- packages/bpk-mixins/sass-functions.js
+++ packages/bpk-mixins/sass-functions.js
@@ -1,9 +1,8 @@
-/* eslint-disable */
-import nodeSass from 'node-sass';
+const nodeSass = require('node-sass');
 
 module.exports = {
-  'encodebase64($string)': function (str) {
-    var buffer = new Buffer(str.getValue());
+  'encodebase64($string)': (str) => {
+    const buffer = new Buffer(str.getValue());
 
     return nodeSass.types.String(buffer.toString('base64'));
   },
