c2cd073c8e538c542ac0a2b614857608ccbe7064
Tim von Oldenburg <tim.vonoldenburg@skyscanner.net>
2017-03-29 16:56:07 +0100

    Add stateful containers to forms docs page

---
+++ packages/bpk-docs/src/pages/FormsPage/InputContainer.js
@@ -0,0 +1,43 @@
+import React, { Component, PropTypes } from 'react';
+import BpkCheckbox from 'bpk-component-checkbox';
+
+class InputContainer extends Component {
+  constructor(props) {
+    super(props);
+
+    const valueProp = props.FormComponent === BpkCheckbox ? 'checked' : 'value';
+
+    this.state = {
+      value: props[valueProp],
+    };
+  }
+
+  render() {
+    const { FormComponent, ...rest } = this.props;
+    let overrideProps = {};
+    if (FormComponent === BpkCheckbox) {
+      overrideProps = {
+        checked: this.state.value,
+        onChange: e => this.setState({ value: e.target.checked }),
+      };
+    } else {
+      overrideProps = {
+        value: this.state.value,
+        onChange: e => this.setState({ value: e.target.value }),
+      };
+    }
+    return (
+      <FormComponent
+        {...rest}
+        {...overrideProps}
+      />
+    );
+  }
+}
+
+InputContainer.propTypes = {
+  FormComponent: PropTypes.func.isRequired,
+  value: PropTypes.string.isRequired,
+};
+
+export default InputContainer;
