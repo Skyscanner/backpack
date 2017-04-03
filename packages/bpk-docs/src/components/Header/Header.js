84ef42cc324419354fb93743616eaf39e1d2378e
James Ferguson <james.ferguson@skyscanner.net>
2017-04-03 17:16:45 +0100

    Removing links in header to WIP pages

--- packages/bpk-docs/src/components/Header/Header.js
+++ packages/bpk-docs/src/components/Header/Header.js
@@ -10,10 +10,9 @@ import * as ROUTES from '../../constants/routes';
 
 const links = [
   { to: ROUTES.GETTING_STARTED_INDEX, children: 'Getting started'},
-  { to: ROUTES.STYLE, children: 'Style'},
   { to: ROUTES.DOCS, children: 'Components' },
-  { to: ROUTES.PATTERNS, children: 'Patterns' },
   { to: ROUTES.RESOURCES, children: 'Resources' },
+  { href: 'http://git.prod.skyscanner.local/backpack/backpack', children: 'GitLab', blank: true },
 ];
 
 const Header = () => (
