ea2d908021cbddbb2b7c7eb7e46d648170b85aca
Matthew Davidson <matthew.davidson@skyscanner.net>
2017-04-07 15:56:57 +0100

    Captialize categories

--- packages/bpk-docs/src/pages/IconsPage/IconsPage.js
+++ packages/bpk-docs/src/pages/IconsPage/IconsPage.js
@@ -1,5 +1,6 @@
 import _ from 'lodash';
 import React from 'react';
+import BpkLink from 'bpk-component-link';
 import icons from 'bpk-component-icon/all';
 import BpkHeading from 'bpk-component-heading';
 import BpkParagraph from 'bpk-component-paragraph';
@@ -16,14 +17,22 @@ const iconsFinal = _(iconsRest)
   .values()
   .flatMap(category => Object.keys(category).map(name => ({
     name,
-    category: name.split('--')[0],
+    category: _.capitalize(name.split('--')[0]),
     component: category[name],
   })))
   .value();
 
 const blurb = [
   <BpkParagraph>
-    TODO
+    Backpack icons are crafted to display across web and native apps. Their clean, solid shapes effortlessly
+    compliment the rest of Backpack.
+  </BpkParagraph>,
+  <BpkParagraph>
+    Using a 24px grid, the icons are balanced against each other and the type ramp. Utility icons
+    in particular will align with the x-height of the type to provide clear lines.
+  </BpkParagraph>,
+  <BpkParagraph>
+    The <BpkLink href="#readme">readme</BpkLink> for the component is available below the search tool.
   </BpkParagraph>,
   <BpkHeading level="h2">Find the right icon</BpkHeading>,
   <IconSearchApp icons={iconsFinal} />,
