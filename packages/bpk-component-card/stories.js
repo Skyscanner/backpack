5a1b9887b4e6cd60d44e283c1693b78972d24881
Tim von Oldenburg <tim.vonoldenburg@skyscanner.net>
2017-04-10 14:32:06 +0100

    Finish vertical version of ticket card

--- packages/bpk-component-card/stories.js
+++ packages/bpk-component-card/stories.js
@@ -26,10 +26,14 @@ storiesOf('bpk-component-card', module)
     <BpkCard stub="Lorem ipsum dolor sit amet.">
       Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum
       sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+      Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum
+      sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
     </BpkCard>
   ))
   .add('Vertical with ticket stub', () => (
     <BpkCard stub="Lorem ipsum dolor sit amet." vertical>
+      Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum
+      sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
       Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum
       sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
     </BpkCard>
