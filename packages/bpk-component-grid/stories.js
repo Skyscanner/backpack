451d32993fd3b0f7362d7e46de272911323ad8d2
Matthew Davidson <matthew.davidson@skyscanner.net>
2017-05-09 13:20:00 +0100

    Removed superfluous `debug` prop from grid component stories

--- packages/bpk-component-grid/stories.js
+++ packages/bpk-component-grid/stories.js
@@ -6,7 +6,7 @@ import { BpkGridContainer, BpkGridRow, BpkGridColumn } from './index';
 storiesOf('bpk-component-grid', module)
   .add('Example', () => (
     <BpkGridContainer debug>
-      <BpkGridRow debug>
+      <BpkGridRow>
         <BpkGridColumn width={1} debug>1</BpkGridColumn>
         <BpkGridColumn width={1} debug>1</BpkGridColumn>
         <BpkGridColumn width={1} debug>1</BpkGridColumn>
@@ -20,7 +20,7 @@ storiesOf('bpk-component-grid', module)
         <BpkGridColumn width={1} debug>1</BpkGridColumn>
         <BpkGridColumn width={1} debug>1</BpkGridColumn>
       </BpkGridRow>
-      <BpkGridRow debug>
+      <BpkGridRow>
         <BpkGridColumn width={2} debug>2</BpkGridColumn>
         <BpkGridColumn width={1} debug>1</BpkGridColumn>
         <BpkGridColumn width={1} debug>1</BpkGridColumn>
@@ -33,7 +33,7 @@ storiesOf('bpk-component-grid', module)
         <BpkGridColumn width={1} debug>1</BpkGridColumn>
         <BpkGridColumn width={1} debug>1</BpkGridColumn>
       </BpkGridRow>
-      <BpkGridRow debug>
+      <BpkGridRow>
         <BpkGridColumn width={3} debug>3</BpkGridColumn>
         <BpkGridColumn width={1} debug>1</BpkGridColumn>
         <BpkGridColumn width={1} debug>1</BpkGridColumn>
@@ -45,7 +45,7 @@ storiesOf('bpk-component-grid', module)
         <BpkGridColumn width={1} debug>1</BpkGridColumn>
         <BpkGridColumn width={1} debug>1</BpkGridColumn>
       </BpkGridRow>
-      <BpkGridRow debug>
+      <BpkGridRow>
         <BpkGridColumn width={4} debug>4</BpkGridColumn>
         <BpkGridColumn width={1} debug>1</BpkGridColumn>
         <BpkGridColumn width={1} debug>1</BpkGridColumn>
@@ -56,7 +56,7 @@ storiesOf('bpk-component-grid', module)
         <BpkGridColumn width={1} debug>1</BpkGridColumn>
         <BpkGridColumn width={1} debug>1</BpkGridColumn>
       </BpkGridRow>
-      <BpkGridRow debug>
+      <BpkGridRow>
         <BpkGridColumn width={5} debug>5</BpkGridColumn>
         <BpkGridColumn width={1} debug>1</BpkGridColumn>
         <BpkGridColumn width={1} debug>1</BpkGridColumn>
@@ -66,7 +66,7 @@ storiesOf('bpk-component-grid', module)
         <BpkGridColumn width={1} debug>1</BpkGridColumn>
         <BpkGridColumn width={1} debug>1</BpkGridColumn>
       </BpkGridRow>
-      <BpkGridRow debug>
+      <BpkGridRow>
         <BpkGridColumn width={6} debug>6</BpkGridColumn>
         <BpkGridColumn width={1} debug>1</BpkGridColumn>
         <BpkGridColumn width={1} debug>1</BpkGridColumn>
@@ -75,7 +75,7 @@ storiesOf('bpk-component-grid', module)
         <BpkGridColumn width={1} debug>1</BpkGridColumn>
         <BpkGridColumn width={1} debug>1</BpkGridColumn>
       </BpkGridRow>
-      <BpkGridRow debug>
+      <BpkGridRow>
         <BpkGridColumn width={7} debug>7</BpkGridColumn>
         <BpkGridColumn width={1} debug>1</BpkGridColumn>
         <BpkGridColumn width={1} debug>1</BpkGridColumn>
@@ -83,87 +83,87 @@ storiesOf('bpk-component-grid', module)
         <BpkGridColumn width={1} debug>1</BpkGridColumn>
         <BpkGridColumn width={1} debug>1</BpkGridColumn>
       </BpkGridRow>
-      <BpkGridRow debug>
+      <BpkGridRow>
         <BpkGridColumn width={8} debug>8</BpkGridColumn>
         <BpkGridColumn width={1} debug>1</BpkGridColumn>
         <BpkGridColumn width={1} debug>1</BpkGridColumn>
         <BpkGridColumn width={1} debug>1</BpkGridColumn>
         <BpkGridColumn width={1} debug>1</BpkGridColumn>
       </BpkGridRow>
-      <BpkGridRow debug>
+      <BpkGridRow>
         <BpkGridColumn width={9} debug>9</BpkGridColumn>
         <BpkGridColumn width={1} debug>1</BpkGridColumn>
         <BpkGridColumn width={1} debug>1</BpkGridColumn>
         <BpkGridColumn width={1} debug>1</BpkGridColumn>
       </BpkGridRow>
-      <BpkGridRow debug>
+      <BpkGridRow>
         <BpkGridColumn width={10} debug>10</BpkGridColumn>
         <BpkGridColumn width={1} debug>1</BpkGridColumn>
         <BpkGridColumn width={1} debug>1</BpkGridColumn>
       </BpkGridRow>
-      <BpkGridRow debug>
+      <BpkGridRow>
         <BpkGridColumn width={11} debug>11</BpkGridColumn>
         <BpkGridColumn width={1} debug>1</BpkGridColumn>
       </BpkGridRow>
-      <BpkGridRow debug>
+      <BpkGridRow>
         <BpkGridColumn width={12} debug>12</BpkGridColumn>
       </BpkGridRow>
-      <BpkGridRow debug>
+      <BpkGridRow>
         <BpkGridColumn width={6} debug>6</BpkGridColumn>
         <BpkGridColumn width={6} debug>6</BpkGridColumn>
       </BpkGridRow>
-      <BpkGridRow debug>
+      <BpkGridRow>
         <BpkGridColumn width={3} debug>3</BpkGridColumn>
         <BpkGridColumn width={3} debug>3</BpkGridColumn>
         <BpkGridColumn width={3} debug>3</BpkGridColumn>
         <BpkGridColumn width={3} debug>3</BpkGridColumn>
       </BpkGridRow>
-      <BpkGridRow debug>
+      <BpkGridRow>
         <BpkGridColumn width={4} debug>4</BpkGridColumn>
         <BpkGridColumn width={4} debug>4</BpkGridColumn>
         <BpkGridColumn width={4} debug>4</BpkGridColumn>
       </BpkGridRow>
-      <BpkGridRow debug>
+      <BpkGridRow>
         <BpkGridColumn width={1} offset={11} debug>1</BpkGridColumn>
       </BpkGridRow>
-      <BpkGridRow debug>
+      <BpkGridRow>
         <BpkGridColumn width={1} offset={10} debug>1</BpkGridColumn>
       </BpkGridRow>
-      <BpkGridRow debug>
+      <BpkGridRow>
         <BpkGridColumn width={1} offset={9} debug>1</BpkGridColumn>
       </BpkGridRow>
-      <BpkGridRow debug>
+      <BpkGridRow>
         <BpkGridColumn width={1} offset={8} debug>1</BpkGridColumn>
       </BpkGridRow>
-      <BpkGridRow debug>
+      <BpkGridRow>
         <BpkGridColumn width={1} offset={7} debug>1</BpkGridColumn>
       </BpkGridRow>
-      <BpkGridRow debug>
+      <BpkGridRow>
         <BpkGridColumn width={1} offset={6} debug>1</BpkGridColumn>
       </BpkGridRow>
-      <BpkGridRow debug>
+      <BpkGridRow>
         <BpkGridColumn width={1} offset={5} debug>1</BpkGridColumn>
       </BpkGridRow>
-      <BpkGridRow debug>
+      <BpkGridRow>
         <BpkGridColumn width={1} offset={4} debug>1</BpkGridColumn>
       </BpkGridRow>
-      <BpkGridRow debug>
+      <BpkGridRow>
         <BpkGridColumn width={1} offset={3} debug>1</BpkGridColumn>
       </BpkGridRow>
-      <BpkGridRow debug>
+      <BpkGridRow>
         <BpkGridColumn width={1} offset={2} debug>1</BpkGridColumn>
       </BpkGridRow>
-      <BpkGridRow debug>
+      <BpkGridRow>
         <BpkGridColumn width={1} offset={1} debug>1</BpkGridColumn>
       </BpkGridRow>
-      <BpkGridRow debug>
+      <BpkGridRow>
         <BpkGridColumn width={1} debug>1</BpkGridColumn>
       </BpkGridRow>
     </BpkGridContainer>
   ))
   .add('Full width example', () => (
     <BpkGridContainer fullWidth debug>
-      <BpkGridRow debug>
+      <BpkGridRow>
         <BpkGridColumn width={1} debug>1</BpkGridColumn>
         <BpkGridColumn width={1} debug>1</BpkGridColumn>
         <BpkGridColumn width={1} debug>1</BpkGridColumn>
@@ -177,7 +177,7 @@ storiesOf('bpk-component-grid', module)
         <BpkGridColumn width={1} debug>1</BpkGridColumn>
         <BpkGridColumn width={1} debug>1</BpkGridColumn>
       </BpkGridRow>
-      <BpkGridRow debug>
+      <BpkGridRow>
         <BpkGridColumn width={2} debug>2</BpkGridColumn>
         <BpkGridColumn width={1} debug>1</BpkGridColumn>
         <BpkGridColumn width={1} debug>1</BpkGridColumn>
@@ -190,7 +190,7 @@ storiesOf('bpk-component-grid', module)
         <BpkGridColumn width={1} debug>1</BpkGridColumn>
         <BpkGridColumn width={1} debug>1</BpkGridColumn>
       </BpkGridRow>
-      <BpkGridRow debug>
+      <BpkGridRow>
         <BpkGridColumn width={3} debug>3</BpkGridColumn>
         <BpkGridColumn width={1} debug>1</BpkGridColumn>
         <BpkGridColumn width={1} debug>1</BpkGridColumn>
@@ -202,7 +202,7 @@ storiesOf('bpk-component-grid', module)
         <BpkGridColumn width={1} debug>1</BpkGridColumn>
         <BpkGridColumn width={1} debug>1</BpkGridColumn>
       </BpkGridRow>
-      <BpkGridRow debug>
+      <BpkGridRow>
         <BpkGridColumn width={4} debug>4</BpkGridColumn>
         <BpkGridColumn width={1} debug>1</BpkGridColumn>
         <BpkGridColumn width={1} debug>1</BpkGridColumn>
@@ -213,7 +213,7 @@ storiesOf('bpk-component-grid', module)
         <BpkGridColumn width={1} debug>1</BpkGridColumn>
         <BpkGridColumn width={1} debug>1</BpkGridColumn>
       </BpkGridRow>
-      <BpkGridRow debug>
+      <BpkGridRow>
         <BpkGridColumn width={5} debug>5</BpkGridColumn>
         <BpkGridColumn width={1} debug>1</BpkGridColumn>
         <BpkGridColumn width={1} debug>1</BpkGridColumn>
@@ -223,7 +223,7 @@ storiesOf('bpk-component-grid', module)
         <BpkGridColumn width={1} debug>1</BpkGridColumn>
         <BpkGridColumn width={1} debug>1</BpkGridColumn>
       </BpkGridRow>
-      <BpkGridRow debug>
+      <BpkGridRow>
         <BpkGridColumn width={6} debug>6</BpkGridColumn>
         <BpkGridColumn width={1} debug>1</BpkGridColumn>
         <BpkGridColumn width={1} debug>1</BpkGridColumn>
@@ -232,7 +232,7 @@ storiesOf('bpk-component-grid', module)
         <BpkGridColumn width={1} debug>1</BpkGridColumn>
         <BpkGridColumn width={1} debug>1</BpkGridColumn>
       </BpkGridRow>
-      <BpkGridRow debug>
+      <BpkGridRow>
         <BpkGridColumn width={7} debug>7</BpkGridColumn>
         <BpkGridColumn width={1} debug>1</BpkGridColumn>
         <BpkGridColumn width={1} debug>1</BpkGridColumn>
@@ -240,87 +240,87 @@ storiesOf('bpk-component-grid', module)
         <BpkGridColumn width={1} debug>1</BpkGridColumn>
         <BpkGridColumn width={1} debug>1</BpkGridColumn>
       </BpkGridRow>
-      <BpkGridRow debug>
+      <BpkGridRow>
         <BpkGridColumn width={8} debug>8</BpkGridColumn>
         <BpkGridColumn width={1} debug>1</BpkGridColumn>
         <BpkGridColumn width={1} debug>1</BpkGridColumn>
         <BpkGridColumn width={1} debug>1</BpkGridColumn>
         <BpkGridColumn width={1} debug>1</BpkGridColumn>
       </BpkGridRow>
-      <BpkGridRow debug>
+      <BpkGridRow>
         <BpkGridColumn width={9} debug>9</BpkGridColumn>
         <BpkGridColumn width={1} debug>1</BpkGridColumn>
         <BpkGridColumn width={1} debug>1</BpkGridColumn>
         <BpkGridColumn width={1} debug>1</BpkGridColumn>
       </BpkGridRow>
-      <BpkGridRow debug>
+      <BpkGridRow>
         <BpkGridColumn width={10} debug>10</BpkGridColumn>
         <BpkGridColumn width={1} debug>1</BpkGridColumn>
         <BpkGridColumn width={1} debug>1</BpkGridColumn>
       </BpkGridRow>
-      <BpkGridRow debug>
+      <BpkGridRow>
         <BpkGridColumn width={11} debug>11</BpkGridColumn>
         <BpkGridColumn width={1} debug>1</BpkGridColumn>
       </BpkGridRow>
-      <BpkGridRow debug>
+      <BpkGridRow>
         <BpkGridColumn width={12} debug>12</BpkGridColumn>
       </BpkGridRow>
-      <BpkGridRow debug>
+      <BpkGridRow>
         <BpkGridColumn width={6} debug>6</BpkGridColumn>
         <BpkGridColumn width={6} debug>6</BpkGridColumn>
       </BpkGridRow>
-      <BpkGridRow debug>
+      <BpkGridRow>
         <BpkGridColumn width={3} debug>3</BpkGridColumn>
         <BpkGridColumn width={3} debug>3</BpkGridColumn>
         <BpkGridColumn width={3} debug>3</BpkGridColumn>
         <BpkGridColumn width={3} debug>3</BpkGridColumn>
       </BpkGridRow>
-      <BpkGridRow debug>
+      <BpkGridRow>
         <BpkGridColumn width={4} debug>4</BpkGridColumn>
         <BpkGridColumn width={4} debug>4</BpkGridColumn>
         <BpkGridColumn width={4} debug>4</BpkGridColumn>
       </BpkGridRow>
-      <BpkGridRow debug>
+      <BpkGridRow>
         <BpkGridColumn width={1} offset={11} debug>1</BpkGridColumn>
       </BpkGridRow>
-      <BpkGridRow debug>
+      <BpkGridRow>
         <BpkGridColumn width={1} offset={10} debug>1</BpkGridColumn>
       </BpkGridRow>
-      <BpkGridRow debug>
+      <BpkGridRow>
         <BpkGridColumn width={1} offset={9} debug>1</BpkGridColumn>
       </BpkGridRow>
-      <BpkGridRow debug>
+      <BpkGridRow>
         <BpkGridColumn width={1} offset={8} debug>1</BpkGridColumn>
       </BpkGridRow>
-      <BpkGridRow debug>
+      <BpkGridRow>
         <BpkGridColumn width={1} offset={7} debug>1</BpkGridColumn>
       </BpkGridRow>
-      <BpkGridRow debug>
+      <BpkGridRow>
         <BpkGridColumn width={1} offset={6} debug>1</BpkGridColumn>
       </BpkGridRow>
-      <BpkGridRow debug>
+      <BpkGridRow>
         <BpkGridColumn width={1} offset={5} debug>1</BpkGridColumn>
       </BpkGridRow>
-      <BpkGridRow debug>
+      <BpkGridRow>
         <BpkGridColumn width={1} offset={4} debug>1</BpkGridColumn>
       </BpkGridRow>
-      <BpkGridRow debug>
+      <BpkGridRow>
         <BpkGridColumn width={1} offset={3} debug>1</BpkGridColumn>
       </BpkGridRow>
-      <BpkGridRow debug>
+      <BpkGridRow>
         <BpkGridColumn width={1} offset={2} debug>1</BpkGridColumn>
       </BpkGridRow>
-      <BpkGridRow debug>
+      <BpkGridRow>
         <BpkGridColumn width={1} offset={1} debug>1</BpkGridColumn>
       </BpkGridRow>
-      <BpkGridRow debug>
+      <BpkGridRow>
         <BpkGridColumn width={1} debug>1</BpkGridColumn>
       </BpkGridRow>
     </BpkGridContainer>
   ))
   .add('Home page example', () => (
     <BpkGridContainer debug>
-      <BpkGridRow debug>
+      <BpkGridRow>
         <BpkGridColumn width={8} tabletWidth={12} debug>
           Search controls
         </BpkGridColumn>
@@ -328,12 +328,12 @@ storiesOf('bpk-component-grid', module)
           MPU
         </BpkGridColumn>
       </BpkGridRow>
-      <BpkGridRow debug>
+      <BpkGridRow>
         <BpkGridColumn width={12} debug>
           Provider logos
         </BpkGridColumn>
       </BpkGridRow>
-      <BpkGridRow debug>
+      <BpkGridRow>
         <BpkGridColumn width={4} mobileWidth={12} debug>
           Content panel
         </BpkGridColumn>
@@ -344,7 +344,7 @@ storiesOf('bpk-component-grid', module)
           Content panel
         </BpkGridColumn>
       </BpkGridRow>
-      <BpkGridRow debug>
+      <BpkGridRow>
         <BpkGridColumn width={3} tabletWidth={6} mobileWidth={12} debug>
           Confidence statement
         </BpkGridColumn>
@@ -362,7 +362,7 @@ storiesOf('bpk-component-grid', module)
   ))
   .add('Day view example', () => (
     <BpkGridContainer debug>
-      <BpkGridRow debug>
+      <BpkGridRow>
         <BpkGridColumn width={9} tabletWidth={12} debug>
           Day view
         </BpkGridColumn>
