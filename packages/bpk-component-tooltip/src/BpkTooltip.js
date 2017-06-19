34085227afbb8b4b26fc3b6943966b743659fef7
Hugo Tunius <hugo.tunius@skyscanner.net>
2017-06-19 11:49:49 +0100

    [BPK-679] TransitionInitialMount CSS Modules
    Remove reliance on classNamePrefix for `TransitionInitialMount`
    so that it correctly works with CSS Modules.

--- packages/bpk-component-tooltip/src/BpkTooltip.js
+++ packages/bpk-component-tooltip/src/BpkTooltip.js
@@ -24,7 +24,11 @@ const BpkTooltip = (props) => {
   if (padded) { innerClassNames.push('bpk-tooltip__inner--padded'); }
 
   return (
-    <TransitionInitialMount classNamePrefix={'bpk-tooltip'} transitionTimeout={200}>
+    <TransitionInitialMount
+      appearClassName="bpk-tooltip--appear"
+      appearActiveClassName="bpk-tooltip--appear-active"
+      transitionTimeout={200}
+    >
       <section
         id={id}
         tabIndex="-1"
