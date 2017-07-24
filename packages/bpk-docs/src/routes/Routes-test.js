9f0054ba5df95168e9f8b59fc579f594dfc1764c
George Gillams <george.gillams@skyscanner.net>
2017-07-24 11:12:04 +0100

    [BPK-807] Fix icons zip bug

---
+++ packages/bpk-docs/src/routes/Routes-test.js
@@ -0,0 +1,13 @@
+import fs from 'fs';
+
+// In order to serve a zip-file containing all icon SVGs, the `zip-it-loader` loader
+// must be provided with an empty file within the Icons directory.
+
+// If Icons.js is not empty, then it will be served instead of the SVGs
+
+describe('Icons.js', () => {
+  it('Icons.js file should be empty', () => {
+    const iconsJsFileContents = fs.readFileSync('packages/bpk-svgs/src/icons/icons.js');
+    expect(iconsJsFileContents.toString()).toEqual('');
+  });
+});
