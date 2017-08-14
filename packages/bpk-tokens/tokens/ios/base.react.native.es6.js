cc35c1414116955046dc921e392c8ef92bb044fd
Hugo Tunius <hugo.tunius@skyscanner.net>
2017-08-14 10:35:27 +0100

    [BPK-861] Add iOS platform tokens

---
+++ packages/bpk-tokens/tokens/ios/base.react.native.es6.js
@@ -0,0 +1,135 @@
+/*
+ * 
+ * Backpack - Skyscanner's Design System
+ * 
+ * Copyright 2017 Skyscanner Ltd
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ */
+export const spacingSm = 4;
+export const spacingMd = 8;
+export const spacingBase = 16;
+export const spacingLg = 24;
+export const spacingXl = 32;
+export const spacingXxl = 40;
+export const fontSizeXs = 11;
+export const fontSizeSm = 13;
+export const fontSizeBase = 15;
+export const fontSizeLg = 17;
+export const fontSizeXl = 20;
+export const fontSizeXxl = 34;
+export const lineHeightXs = 13;
+export const lineHeightSm = 18;
+export const lineHeightBase = 20;
+export const lineHeightLg = 22;
+export const lineHeightXl = 24;
+export const lineHeightXxl = 41;
+export const letterSpacingXs = 0.07;
+export const letterSpacingSm = -0.08;
+export const letterSpacingBase = -0.24;
+export const letterSpacingLg = 0;
+export const letterSpacingXl = 0.38;
+export const letterSpacingXxl = 0.41;
+export const fontWeightNormal = "400";
+export const fontWeightSemibold = "600";
+export const fontWeightBold = "700";
+export const fontWeightHeavy = "800";
+export const textXsFontSize = 11;
+export const textXsFontWeight = "400";
+export const textXsLineHeight = 13;
+export const textXsLetterSpacing = 0.07;
+export const textSmFontSize = 13;
+export const textSmFontWeight = "400";
+export const textSmLineHeight = 18;
+export const textSmLetterSpacing = -0.08;
+export const textBaseFontSize = 15;
+export const textBaseFontWeight = "400";
+export const textBaseLineHeight = 20;
+export const textBaseLetterSpacing = -0.24;
+export const textLgFontSize = 17;
+export const textLgFontWeight = "400";
+export const textLgLineHeight = 22;
+export const textLgLetterSpacing = 0;
+export const textXlFontSize = 20;
+export const textXlFontWeight = "400";
+export const textXlLineHeight = 24;
+export const textXlLetterSpacing = 0.38;
+export const textXxlFontSize = 34;
+export const textXxlFontWeight = "700";
+export const textXxlLineHeight = 41;
+export const textXxlLetterSpacing = 0.41;
+export const spacings = {
+spacingSm,
+spacingMd,
+spacingBase,
+spacingLg,
+spacingXl,
+spacingXxl,
+};
+export const typesettings = {
+fontSizeXs,
+fontSizeSm,
+fontSizeBase,
+fontSizeLg,
+fontSizeXl,
+fontSizeXxl,
+lineHeightXs,
+lineHeightSm,
+lineHeightBase,
+lineHeightLg,
+lineHeightXl,
+lineHeightXxl,
+letterSpacingXs,
+letterSpacingSm,
+letterSpacingBase,
+letterSpacingLg,
+letterSpacingXl,
+letterSpacingXxl,
+};
+export const fontWeights = {
+fontWeightNormal,
+fontWeightSemibold,
+fontWeightBold,
+fontWeightHeavy,
+textXsFontWeight,
+textSmFontWeight,
+textBaseFontWeight,
+textLgFontWeight,
+textXlFontWeight,
+textXxlFontWeight,
+};
+export const fontSizes = {
+textXsFontSize,
+textSmFontSize,
+textBaseFontSize,
+textLgFontSize,
+textXlFontSize,
+textXxlFontSize,
+};
+export const lineHeights = {
+textXsLineHeight,
+textSmLineHeight,
+textBaseLineHeight,
+textLgLineHeight,
+textXlLineHeight,
+textXxlLineHeight,
+};
+export const letterSpacings = {
+textXsLetterSpacing,
+textSmLetterSpacing,
+textBaseLetterSpacing,
+textLgLetterSpacing,
+textXlLetterSpacing,
+textXxlLetterSpacing,
+};
