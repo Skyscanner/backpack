f3f619afb18655e2c3e8d7103352c91c2326cbbc
Shaun Donnelly <shaundonnelly@gmail.com>
2017-09-01 16:53:13 +0100

    [BPK-843] Fixed linter errors
    Also made Jest look in more locations for tests.

--- native/app/index.js
+++ native/app/index.js
@@ -1,1 +1,10 @@
+/* eslint-disable import/no-unresolved */
+
+// ../storybook doesn't contain an index.js file.
+// However, it has index.android.js and index.ios.js.
+// The build process is able to intelligently choose one of these, based
+// on the platform being built.
+// However, eslint doesn't know about this, which is why a rule is disabled
+// in this file.
+
 export default from '../storybook';
--- native/index.ios.js
+++ native/index.ios.js
@@ -6,6 +6,6 @@
 
 import { AppRegistry } from 'react-native';
 
-import App from './app';
+import App from './app/index';
 
 AppRegistry.registerComponent('native', () => App);
