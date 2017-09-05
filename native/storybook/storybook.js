1d93fe222b008fe8f7eaae14dcab77c8ee596780
Shaun Donnelly <shaundonnelly@gmail.com>
2017-09-05 08:40:20 +0100

    [BPK-843] Refactored storybook configs to be less centralised

--- native/storybook/storybook.js
+++ native/storybook/storybook.js
@@ -5,7 +5,8 @@ import { getStorybookUI, configure } from '@storybook/react-native';
 
 // import stories
 configure(() => {
-  require('./stories');
+  // require('./stories');
+  require('../packages/react-native-bpk-component-text/stories');
 }, module);
 
 // This assumes that storybook is running on the same host as your RN packager,
