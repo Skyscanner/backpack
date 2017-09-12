c3bf21e2748cea719749e9852d2b86f885da4289
Elena Petrova <elena.petrova@skyscanner.net>
2017-09-12 18:29:56 +0300

    adds theme selector

--- .storybook/config.js
+++ .storybook/config.js
@@ -6,9 +6,116 @@ import TOKENS from './../packages/bpk-tokens/tokens/base.common';
 import BpkGridToggle from './../packages/bpk-component-grid-toggle';
 import BpkRtlToggle from './../packages/bpk-component-rtl-toggle';
 
+const VARIABLE_NAMES = [
+'primaryColor',
+'primaryContrastColor',
+'primaryDarkColor',
+'primaryDarkContastColor',
+'primaryLightColor',
+'primaryLightContastColor',
+'primaryGradientStartColor',
+'primaryGradientEndColor',
+'secondaryColor',
+'secondaryContrastColor',
+'secondaryDarkColor',
+'secondaryDarkContastColor',
+'secondaryLightColor',
+'secondaryLightContastColor',
+'secondaryGradientStartColor',
+'secondaryGradientEndColor',
+]
+
+const THEMES = {
+  base: {},
+  custom: {
+    primaryColor: '#FFBC2C',
+    primaryContrastColor: '#fff',
+    primaryDarkColor: '#d69814',
+    primaryDarkContastColor: '#fff',
+    primaryLightColor: '#ffd884',
+    primaryLightContastColor: '#4D4D4D',
+    primaryGradientStartColor: '#FFBC2C',
+    primaryGradientEndColor: '#d69814',
+    secondaryColor: '#86B86B',
+    secondaryContrastColor: '#fff',
+    secondaryDarkColor: '#76a85B',
+    secondaryDarkContastColor: '#fff',
+    secondaryLightColor: '#96c87B',
+    secondaryLightContastColor: '#4D4D4D',
+    secondaryGradientStartColor: '#86B86B',
+    secondaryGradientEndColor: '#76a85B',
+  },
+  gazorpazorp: {
+    primaryColor: '#F23557',
+    primaryContrastColor: '#fff',
+    primaryDarkColor: '#bf2944',
+    primaryDarkContastColor: '#fff',
+    primaryLightColor: '#ff6581',
+    primaryLightContastColor: '#3B4A6B',
+    primaryGradientStartColor: '#F23557',
+    primaryGradientEndColor: '#ff6581',
+    secondaryColor: '#F0D43A',
+    secondaryContrastColor: '#3B4A6B',
+    secondaryDarkColor: '#caaf1c',
+    secondaryDarkContastColor: '#3B4A6B',
+    secondaryLightColor: '#f9e264',
+    secondaryLightContastColor: '#3B4A6B',
+    secondaryGradientStartColor: '#F0D43A',
+    secondaryGradientEndColor: '#3B4A6B',
+  }
+};
+
+class ThemeProvider extends React.Component {
+  updateCssVariables() {
+    VARIABLE_NAMES.forEach(variableName => {
+      const cssName = variableName.replace(/([A-Z])/g, function($1){
+        return "-"+$1.toLowerCase();
+      });
+      this.div.style.setProperty(`--${cssName}`, this.props.theme[variableName]);
+    });
+  }
+  componentDidMount() {
+    this.updateCssVariables();
+  }
+  componentDidUpdate() {
+    this.updateCssVariables();
+  }
+  render() {
+    return (<div ref={div => {this.div = div}}>
+      {this.props.children}
+    </div>);
+  }
+}
+
+class ThemeSelector extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      theme: 'base'
+    };
+    this.changeTheme = this.changeTheme.bind(this);
+  }
+  changeTheme(e) {
+    this.setState({
+      theme: e.target.value
+    });
+  }
+  render() {
+    return <div>
+      <label for="select-theme">Select theme:</label>
+      <select name="select-theme" id="select-theme" value={this.state.theme} onChange={this.changeTheme}>
+        {Object.keys(THEMES).map(themeName => <option value={themeName} key={themeName}>{themeName}</option>)}
+      </select>
+      <ThemeProvider theme={THEMES[this.state.theme]}>
+        {this.props.children}
+      </ThemeProvider>
+    </div>
+  }
+}
+
 addDecorator(story => (
   <div style={{ padding: TOKENS.spacingBase }}>
-    {story()}
+    <ThemeSelector>{story()}</ThemeSelector>
     <br />
     <BpkGridToggle />
     <br />
