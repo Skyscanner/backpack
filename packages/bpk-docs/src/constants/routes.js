ac0bf14802fcfc71f2b023a31b27afc51ac1bd3a
George Gillams <george.gillams@skyscanner.net>
2017-09-20 09:49:25 +0100

    [BPK-906] Separate layout docs into 3 pages

--- packages/bpk-docs/src/constants/routes.js
+++ packages/bpk-docs/src/constants/routes.js
@@ -65,6 +65,8 @@ export const HORIZONTAL_NAV = '/components/web/horizontal-nav';
 export const FIELDSETS = '/components/web/fieldsets';
 export const BARCHARTS = '/components/web/barcharts';
 export const STAR_RATING = '/components/web/star-rating';
+export const BREAKPOINTS = '/components/web/breakpoints';
+export const HORIZONTAL_GRID = '/components/web/horizontal-grid';
 
 // components/native/
 export const NATIVE_COMPONENTS = '/components/native';
---
+++ packages/bpk-docs/src/pages/BreakpointsPage/BreakpointsPage.js
@@ -0,0 +1,80 @@
+/*
+ * Backpack - Skyscanner's Design System
+ *
+ * Copyright 2017 Skyscanner Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import React from 'react';
+import PropTypes from 'prop-types';
+import BpkBreakpoint, { BREAKPOINTS } from 'bpk-component-breakpoint';
+import { cssModules } from 'bpk-react-utils';
+import breakpointReadme from 'bpk-component-breakpoint/readme.md';
+
+import DocsPageBuilder from './../../components/DocsPageBuilder';
+import Paragraph from './../../components/Paragraph';
+import STYLES from './breakpoints-page.scss';
+
+const getClassName = cssModules(STYLES);
+
+const MediaQueryStatus = (props) => {
+  const className = getClassName(props.isActive ? 'bpk-breakpoints-demo--active' : 'bpk-breakpoints-demo--inactive');
+
+  return <div className={className}>{props.children}</div>;
+};
+
+MediaQueryStatus.propTypes = {
+  isActive: PropTypes.bool.isRequired,
+  children: PropTypes.node.isRequired,
+};
+
+const components = [
+  {
+    id: 'default',
+    title: 'Default',
+    examples: [
+      <div className={getClassName('bpk-breakpoints-demo')}>
+        <BpkBreakpoint query={BREAKPOINTS.MOBILE}>
+          {isActive => <MediaQueryStatus isActive={isActive}>MOBILE</MediaQueryStatus>}
+        </BpkBreakpoint>
+        <BpkBreakpoint query={BREAKPOINTS.TABLET}>
+          {isActive => <MediaQueryStatus isActive={isActive}>TABLET</MediaQueryStatus>}
+        </BpkBreakpoint>
+        <BpkBreakpoint query={BREAKPOINTS.TABLET_ONLY}>
+          {isActive => <MediaQueryStatus isActive={isActive}>TABLET ONLY</MediaQueryStatus>}
+        </BpkBreakpoint>
+        <BpkBreakpoint query={BREAKPOINTS.ABOVE_MOBILE}>
+          {isActive => <MediaQueryStatus isActive={isActive}>ABOVE MOBILE</MediaQueryStatus>}
+        </BpkBreakpoint>
+        <BpkBreakpoint query={BREAKPOINTS.ABOVE_TABLET}>
+          {isActive => <MediaQueryStatus isActive={isActive}>ABOVE TABLET</MediaQueryStatus>}
+        </BpkBreakpoint>
+      </div>,
+    ],
+  },
+];
+
+const BreakpointsPage = () => <DocsPageBuilder
+  title="Breakpoints"
+  blurb={[
+    <Paragraph>
+      To simplify things, Backpack uses only three breakpoints optimised for mobile, tablet and desktop viewports.
+    </Paragraph>,
+  ]}
+  components={components}
+  readme={breakpointReadme}
+  sassdocId="breakpoints"
+/>;
+
+export default BreakpointsPage;
