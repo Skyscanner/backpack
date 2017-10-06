eb053f46e59e29753aa638fed0fe598be6559976
George333123 <georgegillams@hotmail.co.uk>
2017-10-06 16:19:10 +0100

    [BPK-1004] Add check to ensure all bpk dependencies are latest version

---
+++ scripts/npm/check-bpk-dependencies.js
@@ -0,0 +1,90 @@
+/*
+ * Backpack - Skyscanner's Design System
+ *
+ * Copyright 2017 Skyscanner Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+ /* eslint-disable no-console */
+
+const fs = require('fs');
+const { execSync } = require('child_process');
+
+const errors = [];
+
+const checkBpkDependencyList = (dependencies, correctVersions, packageName) => {
+  Object.keys(dependencies).forEach((dependency) => {
+    if (Object.keys(correctVersions).indexOf(dependency) !== -1) {
+      const dependencyVersion = dependencies[dependency];
+      const correctDependencyVersion = `^${correctVersions[dependency]}`;
+      if (dependencyVersion !== correctDependencyVersion) {
+        // eslint-disable-next-line max-len
+        errors.push(`${packageName} depends on ${dependency} ${dependencyVersion}, it should be ${correctDependencyVersion}`);
+      }
+    }
+  });
+};
+
+const checkBpkDependencies = (packageFile, correctVersions) => {
+  const pfContent = JSON.parse(fs.readFileSync(packageFile));
+  const packageName = pfContent.name;
+  const peerDependencies = pfContent.peerDependencies;
+  const dependencies = pfContent.dependencies;
+  const devDependencies = pfContent.devDependencies;
+
+  if (peerDependencies !== undefined) {
+    checkBpkDependencyList(peerDependencies, correctVersions, packageName);
+  }
+  if (dependencies !== undefined) {
+    checkBpkDependencyList(dependencies, correctVersions, packageName);
+  }
+  if (devDependencies !== undefined) {
+    checkBpkDependencyList(devDependencies, correctVersions, packageName);
+  }
+};
+
+const getBpkPackageVersions = (packageFiles) => {
+  const bpkPackageVersions = {};
+
+  packageFiles.forEach((pf) => {
+    if (pf !== '') {
+      const pfContent = JSON.parse(fs.readFileSync(pf));
+      bpkPackageVersions[pfContent.name] = pfContent.version;
+    }
+  });
+  return bpkPackageVersions;
+};
+
+console.log('Checking Backpack cross dependencies...');
+console.log('');
+
+const packageFiles = execSync('find . -name package.json | grep -v node_modules').toString().split('\n');
+const bpkPackageVersions = getBpkPackageVersions(packageFiles);
+
+packageFiles.forEach((pf) => {
+  if (pf !== '') {
+    checkBpkDependencies(pf, bpkPackageVersions);
+  }
+});
+
+if (errors.length === 0) {
+  console.log('All good.  👍');
+} else {
+  console.log('Some Backpack cross dependencies are outdated  😱');
+  console.log('');
+  errors.forEach((error) => { console.log(`${error}`); });
+  console.log('');
+  process.exit(1);
+}
+console.log('');
