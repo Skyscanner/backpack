49675ec61fee0c79d9e34ecb882ebf69216bd39f
Matthew Davidson <matthew.davidson@skyscanner.net>
2017-11-21 13:23:01 +0000

    [NOJIRA] Fix offending source code

--- native/packages/react-native-bpk-component-card/src/BpkCard-test.android.js
+++ native/packages/react-native-bpk-component-card/src/BpkCard-test.android.js
@@ -30,6 +30,9 @@ jest.mock('react-native', () => {
 
 jest.mock('./BpkCard', () => require.requireActual('./BpkCard.android'));
 
+jest.mock('./../node_modules/react-native-bpk-component-text/node_modules/bpk-tokens/tokens/base.react.native',
+  () => require.requireActual('bpk-tokens/tokens/base.react.native.android.js'));
+
 jest.mock('bpk-tokens/tokens/base.react.native',
   () => require.requireActual('bpk-tokens/tokens/base.react.native.android.js'));
 
