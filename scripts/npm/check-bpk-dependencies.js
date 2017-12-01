c818385720ae7c34c0e3032527c0f0beb772182b
George333123 <georgegillams@hotmail.co.uk>
2017-12-01 15:36:55 +0000

    Add fix option to check-bpk-dependencies script

--- scripts/npm/check-bpk-dependencies.js
+++ scripts/npm/check-bpk-dependencies.js
@@ -23,14 +23,54 @@ const { execSync } = require('child_process');
 
 const errors = [];
 
+const findReplace = (file, findReplaces) => {
+  fs.readFile(file, 'utf8', (err, data) => {
+    if (err) {
+      return console.log(err);
+    }
+    let result = data;
+    findReplaces.forEach((fr) => {
+      const splitFile = result.split(fr.find);
+      if (splitFile.length === 1) {
+        return null;
+      }
+      result = splitFile.join(fr.replace);
+      return null;
+    });
+
+    fs.writeFile(file, result, 'utf8', (err2) => {
+      if (err2) return console.log(err2);
+      return null;
+    });
+    return null;
+  });
+};
+
+const fixDependencyErrors = (packageFiles) => {
+  const findReplaces = [];
+  errors.forEach((error) => {
+    findReplaces.push({
+      find: new RegExp(`\\"${error.dependency}\\"\\:[ ]+\\"\\^[0-9]+\\.[0-9]+\\.[0-9]+\\"`, 'g'),
+      replace: `"${error.dependency}": "${error.correctDependencyVersion}"`,
+    });
+    // eslint-disable-next-line max-len
+    console.log(`${error.dependency} dependency upgraded from ${error.dependencyVersion} to ${error.correctDependencyVersion}`);
+  });
+
+  packageFiles.forEach((file) => {
+    findReplace(file, findReplaces);
+  });
+};
+
 const checkBpkDependencyList = (dependencies, correctVersions, packageName) => {
   Object.keys(dependencies).forEach((dependency) => {
     if (Object.keys(correctVersions).indexOf(dependency) !== -1) {
       const dependencyVersion = dependencies[dependency];
       const correctDependencyVersion = `^${correctVersions[dependency]}`;
       if (dependencyVersion !== correctDependencyVersion) {
-        // eslint-disable-next-line max-len
-        errors.push(`${packageName} depends on ${dependency} ${dependencyVersion}, it should be ${correctDependencyVersion}`);
+        errors.push({
+          packageName, dependency, dependencyVersion, correctDependencyVersion,
+        });
       }
     }
   });
@@ -68,21 +108,26 @@ const getBpkPackageVersions = packageFiles => packageFiles.reduce((acc, pkg) => 
 console.log('Checking Backpack cross dependencies...');
 console.log('');
 
-const packageFiles = execSync('find . -name package.json | grep -v node_modules').toString().split('\n');
+let packageFiles = execSync('find . -name package.json | grep -v node_modules').toString().split('\n');
+packageFiles = packageFiles.filter(s => s !== '');
 const bpkPackageVersions = getBpkPackageVersions(packageFiles);
 
 packageFiles.forEach((pf) => {
-  if (pf !== '') {
-    checkBpkDependencies(pf, bpkPackageVersions);
-  }
+  checkBpkDependencies(pf, bpkPackageVersions);
 });
 
 if (errors.length === 0) {
   console.log('All good.  👍');
+} else if (process.argv.includes('--fix') || process.argv.includes('-f')) {
+  fixDependencyErrors(packageFiles);
+  console.log('\nAll fixed.  👍\n\nNow remember to run\n\t _____________\n\t|             |\n\t| npm install |\n\t|_____________|');
 } else {
   console.log('Some Backpack cross dependencies are outdated  😱');
   console.log('');
-  errors.forEach((error) => { console.log(`${error}`); });
+  errors.forEach((error) => {
+    // eslint-disable-next-line max-len
+    console.log(`${error.packageName} depends on ${error.dependency} ${error.dependencyVersion}, it should be ${error.correctDependencyVersion}`);
+  });
   console.log('');
   process.exit(1);
 }
