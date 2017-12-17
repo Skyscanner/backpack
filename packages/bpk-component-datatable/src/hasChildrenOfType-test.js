7c3f07b6f9b2098835a6378bfe1c423bf7807b71
Janek Lasocki-Biczysko <janek.lasocki-biczysko@skyscanner.net>
2017-12-17 11:42:35 +0000

    [BPK-434] bpk-component-datatable improve propType definition for BpkDataTable children

---
+++ packages/bpk-component-datatable/src/hasChildrenOfType-test.js
@@ -0,0 +1,85 @@
+/*
+ * Backpack - Skyscanner's Design System
+ *
+ * Copyright 2017 Skyscanner Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import React from 'react';
+import hasChildrenOfType, { getDisplayName } from './hasChildrenOfType';
+
+const Component = () => <div>Component</div>;
+const Child = () => <span>Child</span>;
+
+const runPropTypeValidatorWith = (component, atLeast = 1, type = Child) => {
+  const propTypeValidator = hasChildrenOfType(type, atLeast);
+  return propTypeValidator(
+    component.props,
+    'children',
+    getDisplayName(component.type),
+  );
+};
+
+describe('hasChildrenOfType', () => {
+  it('should not return an error if conditions are satisfied', () => {
+    const error = runPropTypeValidatorWith(
+      <Component>
+        <Child />
+      </Component>,
+    );
+    expect(error).toBeUndefined();
+  });
+
+  it('should return an error if it has less children than it should (0 < 1)', () => {
+    const error = runPropTypeValidatorWith(<Component />);
+    expect(error).toEqual(
+      Error("Component requires at least 1 'Child' child."),
+    );
+  });
+
+  it('should return an error if it has less children than it should (1 < 2)', () => {
+    const error = runPropTypeValidatorWith(
+      <Component>
+        <Child />
+      </Component>,
+      2,
+    );
+    expect(error).toEqual(
+      Error("Component requires at least 2 'Child' children."),
+    );
+  });
+
+  it('should return an error if it has single non matching child ', () => {
+    const error = runPropTypeValidatorWith(
+      <Component>
+        <div />
+      </Component>,
+    );
+    expect(error).toEqual(
+      Error("Component only allows 'Child' as children. Found 'div'."),
+    );
+  });
+
+  it('should return an error if it has a mix of matching and non matching children ', () => {
+    const error = runPropTypeValidatorWith(
+      <Component>
+        <Child />
+        <div />
+      </Component>,
+    );
+    expect(error).toEqual(
+      Error("Component only allows 'Child' as children. Found 'div'."),
+    );
+  });
+});
---
+++ packages/bpk-component-datatable/src/hasChildrenOfType.js
@@ -0,0 +1,59 @@
+/*
+ * Backpack - Skyscanner's Design System
+ *
+ * Copyright 2017 Skyscanner Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { Children } from 'react';
+
+export function getDisplayName(Component) {
+  return (
+    Component.displayName ||
+    Component.name ||
+    (typeof Component === 'string' && Component.length > 0
+      ? Component
+      : 'Unknown')
+  );
+}
+
+const hasChildrenOfType = (type, atLeast = 1) => (
+  props,
+  propType,
+  componentName,
+) => {
+  if (Children.count(props[propType]) < atLeast) {
+    const inflectedNoun = atLeast === 1 ? 'child' : 'children';
+    return Error(
+      `${componentName} requires at least ${atLeast} '${getDisplayName(
+        type,
+      )}' ${inflectedNoun}.`,
+    );
+  }
+
+  const children = Children.toArray(props[propType]);
+  for (let i = 0; i < children.length; i += 1) {
+    const child = children[i];
+    if (child.type !== type && !(child.type.prototype instanceof type)) {
+      return Error(
+        `${componentName} only allows '${getDisplayName(type)}' as children. ` +
+          `Found '${getDisplayName(child.type)}'.`,
+      );
+    }
+  }
+
+  return undefined;
+};
+
+export default hasChildrenOfType;
