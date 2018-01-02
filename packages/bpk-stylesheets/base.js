0216327061193041e74e9253ff08e344488aee0f
Matteo Hertel <info@matteohertel.com>
2018-01-02 11:03:01 +0000

    [NoJira] update copyright to 2018

--- packages/bpk-stylesheets/base.js
+++ packages/bpk-stylesheets/base.js
@@ -91,7 +91,7 @@ __webpack_require__(1);
 })(); /*
        * Backpack - Skyscanner's Design System
        *
-       * Copyright 2017 Skyscanner Ltd
+       * Copyright 2018 Skyscanner Ltd
        *
        * Licensed under the Apache License, Version 2.0 (the "License");
        * you may not use this file except in compliance with the License.
