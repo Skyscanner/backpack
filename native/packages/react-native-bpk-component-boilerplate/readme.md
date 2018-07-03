2ea960c6adcd2810d66a820fd5b2d9bf923fb6c4
Shaun Donnelly <shaundonnelly@gmail.com>
2018-07-03 14:18:46 +0100

    [No JIRA] Add boilerplate component for React Native

---
+++ native/packages/react-native-bpk-component-boilerplate/readme.md
@@ -0,0 +1,39 @@
+# react-native-bpk-component-boilerplate
+
+> Backpack React Native boilerplate component.
+
+## Installation
+
+```sh
+npm install react-native-bpk-component-boilerplate --save-dev
+```
+
+## Usage
+
+```js
+import React, { Component } from 'react';
+import { StyleSheet, View } from 'react-native';
+import { spacingBase } from 'bpk-tokens/tokens/base.react.native';
+import BpkBoilerplate from 'react-native-bpk-component-chip';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    padding: spacingBase,
+  },
+});
+
+export default () => (
+  <View style={styles.container}>
+    <BpkBoilerplate />
+  </View>
+);
+```
+
+## Props
+
+### BpkBoilerplate
+
+TODO - Add your component's prop types here.
+
---
+++ native/packages/react-native-bpk-component-boilerplate/src/BpkBoilerplate-test.common.js
@@ -0,0 +1,58 @@
+/*
+ * Backpack - Skyscanner's Design System
+ *
+ * Copyright 2018 Skyscanner Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import React from 'react';
+import { StyleSheet } from 'react-native';
+import renderer from 'react-test-renderer';
+
+import BpkBoilerplate from './BpkBoilerplate';
+
+const commonTests = () => {
+  describe('BpkBoilerplate', () => {
+    it('should render correctly', () => {
+      const tree = renderer.create(<BpkBoilerplate />).toJSON();
+      expect(tree).toMatchSnapshot();
+    });
+
+    it('should render correctly with custom style', () => {
+      const styles = StyleSheet.create({
+        custom: {
+          flex: 1,
+        },
+      });
+
+      const tree = renderer
+        .create(<BpkBoilerplate style={styles.custom} />)
+        .toJSON();
+      expect(tree).toMatchSnapshot();
+    });
+
+    it('should render correctly with arbitrary props', () => {
+      const tree = renderer
+        .create(
+          <BpkBoilerplate
+            testID="123" // <- Arbitrary prop.
+          />,
+        )
+        .toJSON();
+
+      expect(tree).toMatchSnapshot();
+    });
+  });
+};
+export default commonTests;
