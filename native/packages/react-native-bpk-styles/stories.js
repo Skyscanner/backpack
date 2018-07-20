538efaae4c6514b9447a3f6147ec1753b96ad934
Hugo Tunius <h@tunius.se>
2018-07-20 14:04:21 +0100

    [No JIRA] Add performance gradient story

--- native/packages/react-native-bpk-styles/stories.js
+++ native/packages/react-native-bpk-styles/stories.js
@@ -15,8 +15,9 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 /* @flow */
-import React from 'react';
+import React, { Component } from 'react';
 import { storiesOf } from '@storybook/react-native';
 import { StyleSheet, View } from 'react-native';
 import LinearGradient from 'react-native-linear-gradient';
@@ -49,6 +50,63 @@ const styles = StyleSheet.create({
   },
 });
 
+const getRandomColor = () => {
+  const letters = '0123456789ABCDEF';
+  let color = '#';
+
+  for (let i = 0; i < 6; i += 1) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+
+  return color;
+};
+
+type State = {
+  startColor: string,
+  endColor: string,
+};
+class RandomGradient extends Component<{}, State> {
+  state: State;
+
+  intervalId: ?IntervalID;
+
+  constructor() {
+    super();
+
+    this.state = {
+      startColor: getRandomColor(),
+      endColor: getRandomColor(),
+    };
+  }
+
+  componentDidMount() {
+    this.intervalId = setInterval(() => {
+      this.setState(() => ({
+        startColor: getRandomColor(),
+        endColor: getRandomColor(),
+      }));
+    }, 500);
+  }
+
+  componentWillUnmount() {
+    if (this.intervalId) {
+      clearInterval(this.intervalId);
+    }
+  }
+
+  render() {
+    const { startColor, endColor } = this.state;
+
+    return (
+      <LinearGradient
+        style={styles.container}
+        {...gradients.primary()}
+        colors={[startColor, endColor]}
+      />
+    );
+  }
+}
+
 storiesOf('react-native-bpk-styles', module)
   .addDecorator(CenterDecorator)
   .add('docs:gradients', () => (
@@ -66,4 +124,5 @@ storiesOf('react-native-bpk-styles', module)
   ))
   .add('docs:full', () => (
     <LinearGradient style={styles.container} {...gradients.primary()} />
-  ));
+  ))
+  .add('performance:random-gradient', () => <RandomGradient />);
