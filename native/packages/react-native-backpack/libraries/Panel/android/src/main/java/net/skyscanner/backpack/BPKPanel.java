4fd558c874d29f6ee7d0c841ed574ba312e35a69
Amol Gupta <amolgupta1989@gmail.com>
2018-07-25 15:12:12 +0100

    [BPK 1757] android panel scafolding

---
+++ native/packages/react-native-backpack/libraries/Panel/android/src/main/java/net/skyscanner/backpack/BPKPanel.java
@@ -0,0 +1,13 @@
+package net.skyscanner.backpack;
+
+import android.content.Context;
+import android.view.View;
+
+public class BPKPanel extends View {
+
+  public BPKPanel(Context context) {
+    super(context);
+  }
+
+  //TODO implementation
+}
