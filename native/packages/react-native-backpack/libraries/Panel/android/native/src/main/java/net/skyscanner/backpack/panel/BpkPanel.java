ee75cd92a7dbdb9282159d57494199d1aab2caf9
Amol Gupta <amolgupta1989@gmail.com>
2018-07-27 13:25:37 +0100

    panel component implementation for native

--- native/packages/react-native-backpack/libraries/Panel/android/native/src/main/java/net/skyscanner/backpack/panel/BpkPanel.java
+++ native/packages/react-native-backpack/libraries/Panel/android/native/src/main/java/net/skyscanner/backpack/panel/BpkPanel.java
@@ -1,13 +1,49 @@
 package net.skyscanner.backpack.panel;
 
 import android.content.Context;
-import android.view.View;
+import android.content.res.TypedArray;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.widget.FrameLayout;
 
-public class BpkPanel extends View {
+import net.skyscanner.backpack.R;
 
-  public BpkPanel(Context context) {
-    super(context);
-  }
+public class BpkPanel extends FrameLayout {
 
-  //TODO implementation
+    private boolean mPadding;
+
+    public BpkPanel(@NonNull Context context) {
+        super(context);
+    }
+
+    public BpkPanel(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        initialize(context, attrs, defStyleAttr);
+    }
+
+    public BpkPanel(@NonNull Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        initialize(context, attrs, 0);
+    }
+
+    public void setPadding(boolean mPadding) {
+        this.mPadding = mPadding;
+    }
+
+    private void initialize(Context context, AttributeSet attrs, int defStyleAttr) {
+
+        TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.panel, defStyleAttr,
+                R.style.Bpk_panel);
+
+        if (a.hasValue(R.styleable.panel_bpk_panel_padding)) {
+            //default value of padding is true
+            mPadding = a.getBoolean(R.styleable.panel_bpk_panel_padding, true);
+        }
+        int padding = getResources().getDimensionPixelOffset(R.dimen.bpkSpacingSm);
+        if (mPadding)
+            this.setPadding(padding, padding, padding, padding);
+
+        this.setBackground(getResources().getDrawable(R.drawable.border));
+    }
 }
