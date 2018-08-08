e3a250f6befde9a155d22aaefb22ca5013e2db53
Boris Barath <boris.barath16@imperial.ac.uk>
2018-08-08 12:38:33 +0200

    Added tests and updated README.md

--- packages/bpk-component-scrollable-calendar/readme.md
+++ packages/bpk-component-scrollable-calendar/readme.md
@@ -8,6 +8,34 @@
 npm install bpk-component-scrollable-calendar --save-dev
 ```
 
+### BpkScrollableCalendarGrid
+
+The BpkScrollableCalendarGrid component displays a month as a table.
+Above this table it displays the month's name in a `BpkText`. When
+used with `BpkScrollableCalendarDate`, it only displays days from
+that specific month. It is built on top of BpkCalendarGrid and
+uses the same props.
+
+| Property             | PropType | Required | Default Value    |
+| -------------------- | -------- | -------- | ---------------- |
+| DateComponent        | func     | true     | -                |
+| daysOfWeek           | object   | true     | -                |
+| formatDateFull       | func     | true     | -                |
+| formatMonth          | func     | true     | -                |
+| month                | Date     | true     | -                |
+| focusedDate          | Date     | false    | null             |
+| isKeyboardFocusable  | bool     | false    | true             |
+| markOutsideDays      | bool     | false    | true             |
+| markToday            | bool     | false    | true             |
+| maxDate              | Date     | false    | new Date() + 1yr |
+| minDate              | Date     | false    | new Date()       |
+| onDateClick          | func     | false    | null             |
+| onDateKeyDown        | func     | false    | null             |
+| preventKeyboardFocus | bool     | false    | false            |
+| selectedDate         | Date     | false    | null             |
+| showWeekendSeparator | bool     | false    | true             |
+| weekStartsOn         | number   | false    | 1                |
+
 ### BpkScrollableCalendarDate
 
 The BpkScrollableCalendarDate component is used to render the content of a cell
---
+++ packages/bpk-component-scrollable-calendar/src/BpkScrollableCalendarGrid-test.js
@@ -0,0 +1,151 @@
+/*
+ * Backpack - Skyscanner's Design System
+ *
+ * Copyright 2018 Skyscanner Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import PropTypes from 'prop-types';
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { mount } from 'enzyme';
+import isWeekend from 'date-fns/is_weekend';
+
+import { weekDays, formatDateFull, formatMonth } from '../test-utils';
+import BpkCalendarScrollGrid from './BpkScrollableCalendarGrid';
+import BpkCalendarScrollDate from './BpkScrollableCalendarDate';
+import { colorRed500 } from '../../bpk-component-calendar/node_modules/bpk-tokens/tokens/base.es6';
+
+describe('BpkCalendarScrollGrid', () => {
+  it('should render correctly with "showWeekendSeparator" attribute set to false', () => {
+    const tree = renderer
+      .create(
+        <BpkCalendarScrollGrid
+          month={new Date('2016-10')}
+          formatMonth={formatMonth}
+          formatDateFull={formatDateFull}
+          DateComponent={BpkCalendarScrollDate}
+          daysOfWeek={weekDays}
+          weekStartsOn={0}
+          showWeekendSeparator={false}
+        />,
+      )
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('should render correctly with a different "weekStartsOn" attribute', () => {
+    const tree = renderer
+      .create(
+        <BpkCalendarScrollGrid
+          month={new Date('2016-10')}
+          formatMonth={formatMonth}
+          formatDateFull={formatDateFull}
+          DateComponent={BpkCalendarScrollDate}
+          daysOfWeek={weekDays}
+          weekStartsOn={3}
+          showWeekendSeparator
+        />,
+      )
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('should render correctly with a "dateModifiers" attribute', () => {
+    const modifiers = {
+      someClass: () => true,
+    };
+    const tree = renderer
+      .create(
+        <BpkCalendarScrollGrid
+          month={new Date('2016-10')}
+          formatMonth={formatMonth}
+          formatDateFull={formatDateFull}
+          DateComponent={BpkCalendarScrollDate}
+          daysOfWeek={weekDays}
+          weekStartsOn={1}
+          dateModifiers={modifiers}
+          showWeekendSeparator
+        />,
+      )
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('should render correctly with a custom date component', () => {
+    const MyCustomDate = props => {
+      const cx = {
+        backgroundColor: colorRed500,
+        width: '50%',
+        height: '50%',
+        borderRadius: '5rem',
+        margin: '25%',
+      };
+      if (isWeekend(props.date)) {
+        cx.backgroundColor = 'rgb(0, 215, 117)';
+      }
+      return <div style={cx} />;
+    };
+    MyCustomDate.propTypes = {
+      date: PropTypes.instanceOf(Date).isRequired,
+    };
+    const tree = renderer
+      .create(
+        <BpkCalendarScrollGrid
+          month={new Date('2016-10')}
+          formatMonth={formatMonth}
+          formatDateFull={formatDateFull}
+          DateComponent={MyCustomDate}
+          daysOfWeek={weekDays}
+          weekStartsOn={1}
+          showWeekendSeparator
+        />,
+      )
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('should call the onDateClick callback', () => {
+    const onDateClick = jest.fn();
+
+    const grid = mount(
+      <BpkCalendarScrollGrid
+        month={new Date('2016-10')}
+        formatMonth={formatMonth}
+        formatDateFull={formatDateFull}
+        DateComponent={BpkCalendarScrollDate}
+        daysOfWeek={weekDays}
+        weekStartsOn={0}
+        showWeekendSeparator
+        onDateClick={onDateClick}
+      />,
+    );
+
+    expect(onDateClick.mock.calls.length).toBe(0);
+
+    grid
+      .find('button')
+      .at(10)
+      .simulate('click');
+    expect(onDateClick.mock.calls.length).toBe(1);
+    expect(onDateClick.mock.calls[0][0]).toEqual(new Date(2016, 9, 11));
+
+    grid
+      .find('button')
+      .at(11)
+      .simulate('click');
+    expect(onDateClick.mock.calls.length).toBe(2);
+    expect(onDateClick.mock.calls[1][0]).toEqual(new Date(2016, 9, 12));
+  });
+});
