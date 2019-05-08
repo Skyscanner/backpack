01ca228f69b5b472d3359dbfe4ec683b8e297eb6
George Gillams <george.gillams@skyscanner.net>
2019-05-08 16:57:08 +0100

    All the modifications

--- packages/bpk-svgs/dist/js/icons/lg/share.js
+++ packages/bpk-svgs/dist/js/icons/lg/share.js
@@ -1,14 +1,8 @@
-function _objectWithoutProperties(obj, keys) { var target = {}; for (var i in obj) { if (keys.indexOf(i) >= 0) continue; if (!Object.prototype.hasOwnProperty.call(obj, i)) continue; target[i] = obj[i]; } return target; }
-
 import React from "react";
-export default ((_ref) => {
-  let {
-    styles = {}
-  } = _ref,
-      props = _objectWithoutProperties(_ref, ["styles"]);
-
-  return <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" style={{
-    width: "1.5rem",
-    height: "1.5rem"
-  }} {...props}><path d="M13.83 3.12a.5.5 0 0 0-.83.38v3.84a13.18 13.18 0 0 0-5.1 1.89 10.49 10.49 0 0 0-3.39 3.62A13 13 0 0 0 3.13 17 15.38 15.38 0 0 0 3 21v-.14a12.66 12.66 0 0 1 1-2.47 11 11 0 0 1 3.58-3.76 10.57 10.57 0 0 1 2.82-1.33 22.69 22.69 0 0 1 2.6-.64v3.84a.5.5 0 0 0 .84.37L21 10z" /></svg>;
-});
+export default (({
+  styles = {},
+  ...props
+}) => <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" style={{
+  width: "1.5rem",
+  height: "1.5rem"
+}} {...props}><path d="M13.83 3.12a.5.5 0 0 0-.83.38v3.84a13.18 13.18 0 0 0-5.1 1.89 10.49 10.49 0 0 0-3.39 3.62A13 13 0 0 0 3.13 17 15.38 15.38 0 0 0 3 21v-.14a12.66 12.66 0 0 1 1-2.47 11 11 0 0 1 3.58-3.76 10.57 10.57 0 0 1 2.82-1.33 22.69 22.69 0 0 1 2.6-.64v3.84a.5.5 0 0 0 .84.37L21 10z" /></svg>);
--- packages/bpk-svgs/dist/js/icons/sm/send-message.js
+++ packages/bpk-svgs/dist/js/icons/sm/send-message.js
@@ -1,14 +1,8 @@
-function _objectWithoutProperties(obj, keys) { var target = {}; for (var i in obj) { if (keys.indexOf(i) >= 0) continue; if (!Object.prototype.hasOwnProperty.call(obj, i)) continue; target[i] = obj[i]; } return target; }
-
 import React from "react";
-export default ((_ref) => {
-  let {
-    styles = {}
-  } = _ref,
-      props = _objectWithoutProperties(_ref, ["styles"]);
-
-  return <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="18" height="18" style={{
-    width: "1.125rem",
-    height: "1.125rem"
-  }} {...props}><path d="M13.815 20c-.245 0-.49-.123-.736-.368l-2.086-4.54a.782.782 0 0 1 0-.736l3.68-4.907-4.907 3.68c-.245 0-.613 0-.859-.122l-4.539-2.208C4.123 10.676 4 10.43 4 10.062c0-.245.245-.613.49-.613l14.478-5.398c.245-.123.613 0 .858.123.123.368.123.613.123.858L14.551 19.51c-.123.246-.368.491-.736.491.123 0 .123 0 0 0z" /></svg>;
-});
+export default (({
+  styles = {},
+  ...props
+}) => <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="18" height="18" style={{
+  width: "1.125rem",
+  height: "1.125rem"
+}} {...props}><path d="M13.815 20c-.245 0-.49-.123-.736-.368l-2.086-4.54a.782.782 0 0 1 0-.736l3.68-4.907-4.907 3.68c-.245 0-.613 0-.859-.122l-4.539-2.208C4.123 10.676 4 10.43 4 10.062c0-.245.245-.613.49-.613l14.478-5.398c.245-.123.613 0 .858.123.123.368.123.613.123.858L14.551 19.51c-.123.246-.368.491-.736.491.123 0 .123 0 0 0z" /></svg>);
