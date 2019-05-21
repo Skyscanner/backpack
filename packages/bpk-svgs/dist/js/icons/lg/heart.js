6ff42d8f5ffb04bf72781f64aeb9c1c3660bbfd2
Shaun Donnelly <shaundonnelly@gmail.com>
2019-05-21 10:32:15 +0100

    [No JIRA] Update react-svg-loader (#1467)

--- packages/bpk-svgs/dist/js/icons/lg/heart.js
+++ packages/bpk-svgs/dist/js/icons/lg/heart.js
@@ -1,14 +1,8 @@
-function _objectWithoutProperties(obj, keys) { var target = {}; for (var i in obj) { if (keys.indexOf(i) >= 0) continue; if (!Object.prototype.hasOwnProperty.call(obj, i)) continue; target[i] = obj[i]; } return target; }
-
 import React from "react";
-export default ((_ref) => {
-  let {
-    styles = {}
-  } = _ref,
-      props = _objectWithoutProperties(_ref, ["styles"]);
-
-  return <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" style={{
-    width: "1.5rem",
-    height: "1.5rem"
-  }} {...props}><path d="M19.729 5.272a4.963 4.963 0 0 0-6.993.368 5.735 5.735 0 0 0-.736.919 5.735 5.735 0 0 0-.736-.919 4.963 4.963 0 0 0-6.993-.368 4.971 4.971 0 0 0 .368 6.998l3.68 3.683a41.759 41.759 0 0 1 3.438 3.897l.039.051a.258.258 0 0 0 .407 0l.039-.051a41.759 41.759 0 0 1 3.438-3.897l3.68-3.683a4.971 4.971 0 0 0 .368-6.998z" /></svg>;
-});
+export default (({
+  styles = {},
+  ...props
+}) => <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" style={{
+  width: "1.5rem",
+  height: "1.5rem"
+}} {...props}><path d="M19.729 5.272a4.963 4.963 0 0 0-6.993.368 5.735 5.735 0 0 0-.736.919 5.735 5.735 0 0 0-.736-.919 4.963 4.963 0 0 0-6.993-.368 4.971 4.971 0 0 0 .368 6.998l3.68 3.683a41.759 41.759 0 0 1 3.438 3.897l.039.051a.258.258 0 0 0 .407 0l.039-.051a41.759 41.759 0 0 1 3.438-3.897l3.68-3.683a4.971 4.971 0 0 0 .368-6.998z" /></svg>);
--- packages/bpk-svgs/dist/js/icons/lg/minus.js
+++ packages/bpk-svgs/dist/js/icons/lg/minus.js
@@ -1,14 +1,8 @@
-function _objectWithoutProperties(obj, keys) { var target = {}; for (var i in obj) { if (keys.indexOf(i) >= 0) continue; if (!Object.prototype.hasOwnProperty.call(obj, i)) continue; target[i] = obj[i]; } return target; }
-
 import React from "react";
-export default ((_ref) => {
-  let {
-    styles = {}
-  } = _ref,
-      props = _objectWithoutProperties(_ref, ["styles"]);
-
-  return <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" style={{
-    width: "1.5rem",
-    height: "1.5rem"
-  }} {...props}><path d="M19 10a2 2 0 1 1 0 4H5a2 2 0 1 1 0-4h14z" /></svg>;
-});
+export default (({
+  styles = {},
+  ...props
+}) => <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" style={{
+  width: "1.5rem",
+  height: "1.5rem"
+}} {...props}><path d="M19 10a2 2 0 1 1 0 4H5a2 2 0 1 1 0-4h14z" /></svg>);
