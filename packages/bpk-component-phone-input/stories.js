2d3d6bc5b7c211daf7dba80adf0e28704e3f3fbc
Ollie Curtis <8831547+ojcurtis@users.noreply.github.com>
2019-06-10 17:06:20 +0100

    [BPK-2377]: Phone input cuts description (#1445)

--- packages/bpk-component-phone-input/stories.js
+++ packages/bpk-component-phone-input/stories.js
@@ -27,6 +27,7 @@ import BpkPhoneInput from './index';
 
 const DIALING_CODE_TO_ID_MAP = {
   '1': 'us',
+  '2': 'ca',
   '44': 'uk',
   '55': 'br',
   '998': 'uz',
@@ -114,8 +115,9 @@ class StoryContainer extends Component<
           value={value}
           dialingCode={dialingCode}
           dialingCodes={[
-            { code: '1', description: '+1' },
-            { code: '44', description: '+44' },
+            { code: '1', description: '+1 (US)' },
+            { code: '2', description: '+1 (CA)' },
+            { code: '44', description: '+44 (UK)' },
             { code: '55', description: '+55' },
             { code: '998', description: '+998' },
           ]}
