c3be149e4cee5022a9b4a6a9adcd58d995d251d0
Ollie Curtis <8831547+olliecurtis@users.noreply.github.com>
2020-02-27 14:34:09 +0000

    [BPK-3653]: Updated icons to use new spacing

--- packages/bpk-svgs/dist/js/icons/sm/menu--vertical.js
+++ packages/bpk-svgs/dist/js/icons/sm/menu--vertical.js
@@ -2,7 +2,7 @@ import React from "react";
 export default (({
   styles = {},
   ...props
-}) => <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="18" height="18" style={{
-  width: "1.125rem",
-  height: "1.125rem"
+}) => <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="null" height="null" style={{
+  width: "3.5rem/3",
+  height: "3.5rem/3"
 }} {...props}><circle cx="12" cy="5" r="2" /><circle cx="12" cy="12" r="2" /><circle cx="12" cy="19" r="2" /></svg>);
