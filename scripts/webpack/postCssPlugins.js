b1f6e31ec235109f96cf889f6007e99a04c028df
Shaun Donnelly <shaundonnelly@gmail.com>
2020-02-27 16:57:02 +0000

    [squash] Some more

--- scripts/webpack/postCssPlugins.js
+++ scripts/webpack/postCssPlugins.js
@@ -22,7 +22,12 @@ const postCssFlexbugsFixes = require('postcss-flexbugs-fixes');
 module.exports = () => [
   postCssFlexbugsFixes,
   autoprefixer({
-    browsers: ['>1%', 'last 4 versions', 'Firefox ESR', 'not ie < 9'],
+    overrideBrowserslist: [
+      '>1%',
+      'last 4 versions',
+      'Firefox ESR',
+      'not ie < 9',
+    ],
     flexbox: 'no-2009',
   }),
 ];
