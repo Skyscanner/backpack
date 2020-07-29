4499fe5854e6ada0ab3d67ca1248498b7a4b9edc
George Gillams <george.gillams@skyscanner.net>
2020-07-29 19:06:57 +0100

    [BPK-4073] Update auto-generated files

--- packages/bpk-svgs/dist/js/icons/sm/native-android--back.js
+++ packages/bpk-svgs/dist/js/icons/sm/native-android--back.js
@@ -5,4 +5,4 @@ export default (({
 }) => <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="18" height="18" style={{
   width: "1.125rem",
   height: "1.125rem"
-}} {...props}><path fillRule="evenodd" d="M21 12a1.5 1.5 0 0 1-1.5 1.5H8.121l4.94 4.94a1.5 1.5 0 0 1-2.122 2.12L2.38 12l8.56-8.56a1.5 1.5 0 0 1 2.122 2.12L8.12 10.5H19.5A1.5 1.5 0 0 1 21 12z" clipRule="evenodd" /></svg>);
+}} {...props}><path fillRule="evenodd" d="M12.707 19.713a1 1 0 0 1-1.414 0l-7-7a1 1 0 0 1 0-1.414l7-7a1 1 0 0 1 1.438 1.39l-.024.024-5.293 5.293H19a1 1 0 0 1 0 2H7.414l5.293 5.293a1 1 0 0 1 0 1.414z" /></svg>);
