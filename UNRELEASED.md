8cb7654c36c0303a7e03ac2c61f312a0a307ecf4
George Gillams <george.gillams@skyscanner.net>
2020-12-02 15:43:42 +0000

    [NO-JIRA] Fix consumer rowClassName

--- UNRELEASED.md
+++ UNRELEASED.md
@@ -1,6 +1,9 @@
 # Unreleased
 
 > Place your changes below this line.
+**Fixed:**
+ - bpk-component-datatable:
+   - `rowClassName` will now be applied to the row properly.
 
 ## How to write a good changelog entry
 
--- packages/bpk-component-datatable/src/BpkDataTable-test.js
+++ packages/bpk-component-datatable/src/BpkDataTable-test.js
@@ -106,6 +106,49 @@ describe('BpkDataTable', () => {
     expect(tree).toMatchSnapshot();
   });
 
+  it('should render correctly with a custom rowClassName', () => {
+    const tree = renderer
+      .create(
+        <BpkDataTable
+          rows={rows}
+          height={200}
+          rowClassName="custom-data-table__row"
+        >
+          <BpkDataTableColumn label="Name" dataKey="name" width={100} />
+          <BpkDataTableColumn
+            label="Description"
+            dataKey="description"
+            width={100}
+            flexGrow={1}
+          />
+          <BpkDataTableColumn label="Bla" dataKey="bla" width={100} />
+        </BpkDataTable>,
+      )
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('should render correctly with a custom headerClassName', () => {
+    const tree = renderer
+      .create(
+        <BpkDataTable
+          rows={rows}
+          height={200}
+          headerClassName="custom-data-table__header"
+        >
+          <BpkDataTableColumn label="Name" dataKey="name" width={100} />
+          <BpkDataTableColumn
+            label="Description"
+            dataKey="description"
+            width={100}
+            flexGrow={1}
+          />
+          <BpkDataTableColumn label="Bla" dataKey="bla" width={100} />
+        </BpkDataTable>,
+      )
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
   it('should render correctly with a custom className', () => {
     const tree = renderer
       .create(
