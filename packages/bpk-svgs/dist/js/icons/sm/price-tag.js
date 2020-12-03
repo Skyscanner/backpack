874f248f2a24f5ec3698bad66a931970b597cef0
George Gillams <george.gillams@skyscanner.net>
2020-12-03 17:29:08 +0000

    [BPKR-164] Autogenerate icons

---
+++ packages/bpk-svgs/dist/js/icons/sm/price-tag.js
@@ -0,0 +1,8 @@
+import React from "react";
+export default (({
+  styles = {},
+  ...props
+}) => <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="18" height="18" style={{
+  width: "1.125rem",
+  height: "1.125rem"
+}} {...props}><path fillRule="evenodd" d="M12.386 2.996a2.608 2.608 0 0 1 2.608 2.609v3.191a1.5 1.5 0 0 1-.439 1.06l-4.92 4.922a1.5 1.5 0 0 1-2.122 0l-4.3-4.3a1.5 1.5 0 0 1 0-2.121l4.921-4.921a1.5 1.5 0 0 1 1.06-.44zm-.983 3.593a1 1 0 1 1 1.413-1.414 1 1 0 0 1-1.413 1.414z" /></svg>);
