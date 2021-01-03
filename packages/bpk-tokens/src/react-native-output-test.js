8d365c0137e7e981462abc743deb341c909c98a8
George Gillams <george.gillams@skyscanner.net>
2021-01-03 18:36:57 +0000

    [NO-JIRA] Update license headers 2021

--- packages/bpk-tokens/src/react-native-output-test.js
+++ packages/bpk-tokens/src/react-native-output-test.js
@@ -2,7 +2,7 @@
  *
  * Backpack - Skyscanner's Design System
  *
- * Copyright 2016-2020 Skyscanner Ltd
+ * Copyright 2016-2021 Skyscanner Ltd
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
