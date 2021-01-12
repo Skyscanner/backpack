8c55f52b619925779b79f592d0f6c54dd4e846b3
Ollie Curtis <8831547+olliecurtis@users.noreply.github.com>
2021-01-12 10:42:48 +0000

    Add missing text colours

--- packages/bpk-tokens/tokens/base.es6.js
+++ packages/bpk-tokens/tokens/base.es6.js
@@ -405,12 +405,18 @@ export const textLgFontSize = "1.25rem";
 export const textLgFontWeight = "400";
 export const textLgLetterSpacing = "normal";
 export const textLgLineHeight = "1.625rem";
+export const textPrimaryDarkColor = "rgb(255, 255, 255)";
 export const textPrimaryLightColor = "rgb(17, 18, 54)";
+export const textQuaternaryDarkColor = "rgb(142, 142, 147)";
+export const textQuaternaryLightColor = "rgb(143, 144, 160)";
+export const textSecondaryDarkColor = "rgb(142, 142, 147)";
 export const textSecondaryLightColor = "rgb(68, 69, 96)";
 export const textSmFontSize = ".875rem";
 export const textSmFontWeight = "400";
 export const textSmLetterSpacing = "normal";
 export const textSmLineHeight = "1.125rem";
+export const textTertiaryDarkColor = "rgb(142, 142, 147)";
+export const textTertiaryLightColor = "rgb(143, 144, 160)";
 export const textXlFontSize = "1.5rem";
 export const textXlFontWeight = "400";
 export const textXlLetterSpacing = "normal";
@@ -884,8 +890,14 @@ linkWhiteActiveColor,
 linkWhiteColor,
 linkWhiteHoverColor,
 linkWhiteVisitedColor,
+textPrimaryDarkColor,
 textPrimaryLightColor,
+textQuaternaryDarkColor,
+textQuaternaryLightColor,
+textSecondaryDarkColor,
 textSecondaryLightColor,
+textTertiaryDarkColor,
+textTertiaryLightColor,
 };
 export const textDecorations = {
 linkHoverTextDecoration,
