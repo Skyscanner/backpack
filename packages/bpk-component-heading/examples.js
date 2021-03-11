1b2ba29971a61f95c3cea4f2dcab5bfe163a4aef
Ollie Curtis <8831547+olliecurtis@users.noreply.github.com>
2021-03-11 16:24:32 +0000

    [BPK-4389]: Migrate two components to new stories (#2116)

--- packages/bpk-component-heading/examples.js
+++ packages/bpk-component-heading/examples.js
@@ -18,8 +18,19 @@
 
 /* @flow strict */
 
-import { storiesOf } from '@storybook/react';
+import React from 'react';
 
-import Examples from './examples';
+import BpkHeading from './index';
 
-storiesOf('bpk-component-heading', module).add('Examples', Examples);
+const Examples = () => (
+  <div>
+    <BpkHeading level="h1">Heading 1</BpkHeading>
+    <BpkHeading level="h2">Heading 2</BpkHeading>
+    <BpkHeading level="h3">Heading 3</BpkHeading>
+    <BpkHeading level="h4">Heading 4</BpkHeading>
+    <BpkHeading level="h5">Heading 5</BpkHeading>
+    <BpkHeading level="h6">Heading 6</BpkHeading>
+  </div>
+);
+
+export default Examples;
