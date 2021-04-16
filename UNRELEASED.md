dafbe009f2ade76ca054bdb950deb70d9a72ad86
Oliver Curtis <8831547+olliecurtis@users.noreply.github.com>
2021-04-16 16:29:04 +0100

    [KOA-4472]: Enhance slider accessibility

---
+++ UNRELEASED.md
@@ -0,0 +1,3 @@
+**Fixed:**
+ - bpk-component-slider:
+  - Updated to latest `react-slider` version which contains new A11y fixes.
--- packages/bpk-component-slider/src/accessibility-test.js
+++ packages/bpk-component-slider/src/accessibility-test.js
@@ -28,18 +28,36 @@ describe('BpkSlider accessibility tests', () => {
   we're using doesn't pass down the ARIA props we add.
   We should investigate and fix this so we can reinstate the test.
   */
-  it.skip('should not have programmatically-detectable accessibility issues', async () => {
+  it('should not have programmatically-detectable accessibility issues', async () => {
     const { container } = render(
       <div>
         <label id="label" htmlFor="slider">
           Slider
         </label>
         <BpkSlider
-          aria-labelledby="label"
+          ariaLabelledby="label"
           id="slider"
           min={0}
           max={100}
           value={25}
+        />
+      </div>,
+    );
+    const results = await axe(container);
+    expect(results).toHaveNoViolations();
+  });
+
+  it('should not have programmatically-detectable accessibility issues when using a range slider', async () => {
+    const { container } = render(
+      <div>
+        <label id="range-label" htmlFor="range-slider">
+          Range Slider
+        </label>
+        <BpkSlider
+          ariaLabelledby="range-label"
+          id="range-slider"
+          min={0}
+          value={[20, 80]}
         />
       </div>,
     );
