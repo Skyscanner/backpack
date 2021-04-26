f0f28b06fd31138f5105e0596f623f1ddc669984
Oliver Curtis <8831547+olliecurtis@users.noreply.github.com>
2021-04-26 12:18:55 +0100

    Minor updates to examples

--- UNRELEASED.md
+++ UNRELEASED.md
@@ -1,4 +1,3 @@
 **Added:**
 - bpk-component-nudger:
-  - Fixed A11y jest axe test
   - Added ability to spread `rest` across the input component to add `aria` and other `input` props.
--- packages/bpk-component-nudger/examples.js
+++ packages/bpk-component-nudger/examples.js
@@ -54,11 +54,11 @@ class NudgerContainer extends Component<
 
     return (
       <div>
-        <BpkLabel htmlFor={id} className={labelClassName}>
+        <BpkLabel id="passenger-label" htmlFor={id} className={labelClassName}>
           Number of passengers
         </BpkLabel>
         <BpkNudger
-          aria-label={`${this.state.value} passengers`}
+          aria-labelledby="passenger-label"
           id={id}
           min={1}
           max={10}
@@ -114,9 +114,12 @@ class ConfigurableNudgerContainer extends Component<{}, { value: string }> {
   render() {
     return (
       <div>
-        <BpkLabel htmlFor="nudger">Traveller Class</BpkLabel>
+        <BpkLabel id="traveller-label" htmlFor="nudger">
+          Traveller Class
+        </BpkLabel>
         <BpkConfigurableNudger
           id="nudger"
+          aria-labelledby="traveller-label"
           min="economy"
           max="first"
           value={this.state.value}
