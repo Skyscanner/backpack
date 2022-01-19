ecb4e8666b5fddbaef9699956ee28c0cd1a004b4
Oliver Curtis <8831547+olliecurtis@users.noreply.github.com>
2022-01-19 17:14:28 +0000

    reverting files during dangerfile testing

--- dangerfile.js
+++ dangerfile.js
@@ -117,8 +117,8 @@ if (unlicensedFiles.length > 0) {
 const nonModuleCssFiles = fileChanges.filter(
   filePath =>
     filePath.match(/bpk-component/) &&
-    !filePath.startsWith('/_/') &&
     filePath.match(/\.s?css/) &&
+    !filePath.match('_') &&
     !filePath.match(/\.module\.s?css/),
 );
 if (nonModuleCssFiles.length) {
