f437c468e9325f70aeaf3c66423d5941aad1e14a
Zhivko Georgiev <zhivko.georgiev@skyscanner.net>
2022-02-08 13:14:31 +0000

    SB migration

--- .storybook/main.js
+++ .storybook/main.js
@@ -16,8 +16,21 @@
  * limitations under the License.
  */
 
-// TODO: addon-a11y breaks the calendar example
-import '@storybook/addon-a11y/register';
-import '@storybook/addon-actions/register';
-import '@storybook/addon-knobs/register';
-import '@storybook/addon-viewport/register';
+module.exports = {
+//   webpackFinal: async (config, { configType }) => {
+//     config.optimization = {
+//       minimize: false,
+//       minimizer: [],
+//     };
+
+//     return config;
+//   },
+  stories: ['../packages/backpack-component-accordion/stories.js'],
+  addons: [
+    '@storybook/addon-a11y',
+    '@storybook/addon-actions',
+    '@storybook/addon-knobs',
+    '@storybook/addon-viewport',
+  ],
+  
+};
