85e1a717511ba2cc584b3691f621125753d8da06
Alakbar <alakbar.zeynalzade@skyscanner.net>
2022-03-14 15:26:26 +0000

    Added accessibilty tests

--- packages/bpk-component-graphic-promo/src/accessibility-test.js
+++ packages/bpk-component-graphic-promo/src/accessibility-test.js
@@ -24,8 +24,106 @@ import { axe } from 'jest-axe';
 import BpkGraphicPromo from './BpkGraphicPromo';
 
 describe('BpkGraphicPromo accessibility tests', () => {
+  const image = './path/to/image.jpg';
+
   it('should not have programmatically-detectable accessibility issues', async () => {
-    const { container } = render(<BpkGraphicPromo />);
+    const { container } = render(
+      <BpkGraphicPromo
+        kicker="Kicker"
+        headline="Headline"
+        strapline="Strapline"
+        image="https://content.skyscnr.com/m/4933408c167ee099/original/MERCH-131-Graphic-Promo-2096x800.png"
+        sponsorLogo="https://www.easyjet.com/ejcms/cache/medialibrary/Images/Global/Main-III/Logo/ej-transparent-x.png"
+        sponsorAltText="easyJet"
+        ctaText="Learn More"
+        textAlign="start"
+        invertVertically={false}
+      />,
+    );
+    const results = await axe(container);
+    expect(results).toHaveNoViolations();
+  });
+
+  it('should not have programmatically-detectable accessibility issues in centre aligned', async () => {
+    const { container } = render(
+      <BpkGraphicPromo
+        kicker="Kicker"
+        headline="Headline"
+        strapline="Strapline"
+        image="https://content.skyscnr.com/m/4933408c167ee099/original/MERCH-131-Graphic-Promo-2096x800.png"
+        sponsorLogo="https://www.easyjet.com/ejcms/cache/medialibrary/Images/Global/Main-III/Logo/ej-transparent-x.png"
+        sponsorAltText="easyJet"
+        ctaText="Learn More"
+        textAlign="center"
+        invertVertically={false}
+      />,
+    );
+    const results = await axe(container);
+    expect(results).toHaveNoViolations();
+  });
+
+  it('should not have programmatically-detectable accessibility issues in right aligned', async () => {
+    const { container } = render(
+      <BpkGraphicPromo
+        kicker="Kicker"
+        headline="Headline"
+        strapline="Strapline"
+        image="https://content.skyscnr.com/m/4933408c167ee099/original/MERCH-131-Graphic-Promo-2096x800.png"
+        sponsorLogo="https://www.easyjet.com/ejcms/cache/medialibrary/Images/Global/Main-III/Logo/ej-transparent-x.png"
+        sponsorAltText="easyJet"
+        ctaText="Learn More"
+        textAlign="end"
+        invertVertically={false}
+      />,
+    );
+    const results = await axe(container);
+    expect(results).toHaveNoViolations();
+  });
+
+  it('should not have programmatically-detectable accessibility issues in inverted portrait', async () => {
+    const { container } = render(
+      <BpkGraphicPromo
+        kicker="Kicker"
+        headline="Headline"
+        strapline="Strapline"
+        image="https://content.skyscnr.com/m/4933408c167ee099/original/MERCH-131-Graphic-Promo-2096x800.png"
+        sponsorLogo="https://www.easyjet.com/ejcms/cache/medialibrary/Images/Global/Main-III/Logo/ej-transparent-x.png"
+        ctaText="Learn More"
+        textAlign="start"
+        invertVertically
+      />,
+    );
+    const results = await axe(container);
+    expect(results).toHaveNoViolations();
+  });
+
+  it('should not have programmatically-detectable accessibility issues in minimalistic', async () => {
+    const { container } = render(
+      <BpkGraphicPromo
+        headline="Headline"
+        image="https://content.skyscnr.com/m/4933408c167ee099/original/MERCH-131-Graphic-Promo-2096x800.png"
+        sponsorLogo="https://www.easyjet.com/ejcms/cache/medialibrary/Images/Global/Main-III/Logo/ej-transparent-x.png"
+        ctaText="Learn More"
+        textAlign="start"
+        invertVertically={false}
+      />,
+    );
+    const results = await axe(container);
+    expect(results).toHaveNoViolations();
+  });
+
+  it('should not have programmatically-detectable accessibility issues in non-sponsored', async () => {
+    const { container } = render(
+      <BpkGraphicPromo
+        kicker="Kicker"
+        headline="Headline"
+        strapline="Strapline"
+        image="https://content.skyscnr.com/m/4933408c167ee099/original/MERCH-131-Graphic-Promo-2096x800.png"
+        ctaText="Learn More"
+        textAlign="start"
+        invertVertically={false}
+      />,
+    );
     const results = await axe(container);
     expect(results).toHaveNoViolations();
   });
