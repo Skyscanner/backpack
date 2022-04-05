7e13970cb7485d1bdb6ebad8da85d469bab49473
Ana Belciug <ana.belciug@skyscanner.net>
2022-04-05 14:30:21 +0100

    Add new button types

--- packages/bpk-component-button/src/themeAttributes.js
+++ packages/bpk-component-button/src/themeAttributes.js
@@ -29,6 +29,24 @@ export const primaryThemeAttributes = [
   'buttonPrimaryActiveBackgroundColor',
 ];
 
+export const primaryOnDarkThemeAttributes = [
+  'buttonPrimaryOnDarkTextColor',
+  'buttonPrimaryOnDarkHoverTextColor',
+  'buttonPrimaryOnDarkActiveTextColor',
+  'buttonPrimaryOnDarkBackgroundColor',
+  'buttonPrimaryOnDarkHoverBackgroundColor',
+  'buttonPrimaryOnDarkActiveBackgroundColor',
+];
+
+export const primaryOnLightThemeAttributes = [
+  'buttonPrimaryOnLightTextColor',
+  'buttonPrimaryOnLightHoverTextColor',
+  'buttonPrimaryOnLightActiveTextColor',
+  'buttonPrimaryOnLightBackgroundColor',
+  'buttonPrimaryOnLightHoverBackgroundColor',
+  'buttonPrimaryOnLightActiveBackgroundColor',
+];
+
 export const secondaryThemeAttributes = [
   'buttonSecondaryTextColor',
   'buttonSecondaryHoverTextColor',
