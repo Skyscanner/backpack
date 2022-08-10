1f2fa828ff91af79a7c24269ca6217d6026dcf4e
Kirsty <kirsty.bryce@skyscanner.net>
2022-08-10 16:32:40 +0200

    [NEUT-599] update a11y test and snapshot

--- packages/bpk-component-content-cards/src/accessibility-test.js
+++ packages/bpk-component-content-cards/src/accessibility-test.js
@@ -23,9 +23,36 @@ import { axe } from 'jest-axe';
 
 import BpkContentCards from './BpkContentCards';
 
+const headerText = 'Header for the component';
+const cards = [
+  {
+    imageLink: 'http://skyscanner.net/card-1-image-link.jpg',
+    imageAlt: '',
+    headline: 'Card 1 headline',
+    description: 'Card 1 description',
+    ctaLink: 'http://skyscanner.net/card-1-cta-link.html',
+  },
+  {
+    imageLink: 'http://skyscanner.net/card-2-image-link.jpg',
+    imageAlt: '',
+    headline: 'Card 2 headline',
+    description: 'Card 2 description',
+    ctaLink: 'http://skyscanner.net/card-2-cta-link.html',
+  },
+  {
+    imageLink: 'http://skyscanner.net/card-3-image-link.jpg',
+    imageAlt: '',
+    headline: 'Card 3 headline',
+    description: 'Card 3 description',
+    ctaLink: 'http://skyscanner.net/card-3-cta-link.html',
+  },
+];
+
 describe('BpkContentCards accessibility tests', () => {
   it('should not have programmatically-detectable accessibility issues', async () => {
-    const { container } = render(<BpkContentCards />);
+    const { container } = render(
+      <BpkContentCards headerText={headerText} cards={cards} />,
+    );
     const results = await axe(container);
     expect(results).toHaveNoViolations();
   });
