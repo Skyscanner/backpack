a8bc09a8f80995e277cd91c056dcfd77ed4f90d3
Aleksandr Sannikov <aleksandr.sannikov@skyscanner.net>
2022-08-15 16:26:39 +0100

    ARGG-211: Fixed babel.config to run tests

--- babel.config.js
+++ babel.config.js
@@ -41,45 +41,55 @@ module.exports = {
   plugins: [
     '@babel/plugin-proposal-object-rest-spread',
     '@babel/plugin-proposal-class-properties',
-    [
-      'module-resolver',
-      {
-        root: ['./packages'],
-        alias: {
-          '^bpk-(.+)': ([name]) => `../${name}`,
-          '@skyscanner/bpk-foundations-web/tokens/base.es6':
-            '@skyscanner/bpk-foundations-web/tokens/base.common',
-          '^@skyscanner/bpk-svgs/dist/js/(.+)': (stats) =>
-            `@skyscanner/bpk-svgs/dist/svgs/${stats[1]}.svg`,
-          'd3-array': './d3-array/src',
-          'd3-color': './d3-color/src',
-          'd3-format': './d3-format/src',
-          'd3-interpolate': './d3-interpolate/src',
-          'd3-scale': './d3-scale/src',
-          'd3-time': './d3-time/src',
-          'd3-time-format': './d3-time-format/src',
-          internmap: './internmap/src',
-          'react-virtualized': 'react-virtualized/dist/commonjs',
-        },
-      },
-    ],
-    [
-      'css-modules-transform',
-      {
-        extensions: ['.scss', '.css'],
-        preprocessCss: '../../scripts/transpilation/preprocess-css.js',
-        extractCss: './dist/index.css',
-      },
-    ],
-    'inline-react-svg',
   ],
-  ignore: [
-    (fileName) =>
-      !!(
-        fileName.includes('-test') ||
-        fileName.includes('-Test') ||
-        fileName.includes('dist') ||
-        /\/node_modules\//.test(fileName)
-      ),
-  ],
+  env: {
+    dev: {
+      plugins: [
+        [
+          'module-resolver',
+          {
+            root: ['./packages'],
+            alias: {
+              '^bpk-(.+)': ([name]) => `../${name}`,
+              '@skyscanner/bpk-foundations-web/tokens/base.es6':
+                '@skyscanner/bpk-foundations-web/tokens/base.common',
+              '^@skyscanner/bpk-svgs/dist/js/(.+)': (stats) =>
+                `@skyscanner/bpk-svgs/dist/svgs/${stats[1]}.svg`,
+              'd3-array': './d3-array/src',
+              'd3-color': './d3-color/src',
+              'd3-format': './d3-format/src',
+              'd3-interpolate': './d3-interpolate/src',
+              'd3-scale': './d3-scale/src',
+              'd3-time': './d3-time/src',
+              'd3-time-format': './d3-time-format/src',
+              internmap: './internmap/src',
+              'react-virtualized': 'react-virtualized/dist/commonjs',
+            },
+          },
+        ],
+        [
+          'css-modules-transform',
+          {
+            extensions: ['.scss', '.css'],
+            preprocessCss: '../../scripts/transpilation/preprocess-css.js',
+            extractCss: './dist/index.css',
+          },
+        ],
+        'inline-react-svg',
+      ],
+      ignore: [
+        (fileName) =>
+          !!(
+            fileName.includes('-test') ||
+            fileName.includes('-Test') ||
+            fileName.includes('dist') ||
+            /\/node_modules\//.test(fileName)
+          ),
+      ],
+    },
+    test: {
+      // Prevent errors from require.context not existing when running Storyshots.
+      plugins: ['require-context-hook'],
+    },
+  },
 };
