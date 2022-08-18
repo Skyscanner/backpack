b80ee212e5d25b9e0fdda6bcfb53b10d2252c01a
Dominic Fraser <dominic.fraser@skyscanner.net>
2022-08-18 22:46:21 +0100

    typecheck doesn't need a capital T

--- @types/backpack-stubs.d.ts
+++ @types/backpack-stubs.d.ts
@@ -2,7 +2,6 @@
 Normally we'd have a declaration file per module. Here, however, we have all packages that are internal to this repo but not yet converted to TypeScript stubbed in one file. As each component is converted it can be removed from this list, and this file ultimately deleted.
 */
 
-declare module 'bpk-component-button';
 declare module 'bpk-component-text';
 declare module 'bpk-react-utils';
 declare module 'bpk-component-button';
