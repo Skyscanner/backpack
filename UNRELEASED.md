e794229ee774c0753e223dae208ac56e5fab1dea
Ana Belciug <anabelciug@skyscanner.net>
2022-09-27 17:15:31 +0100

    Update tables to semantic colours

---
+++ UNRELEASED.md
@@ -0,0 +1,9 @@
+**Changed:**
+
+- bpk-component-table:
+    - Deprecate `alternate` property.
+    - Update component to semantic colours
+
+`bpk-component-datatable`: </br>
+`bpk-component-content-container`: </br>
+    - Update components to semantic colours
--- examples/bpk-component-table/examples.js
+++ examples/bpk-component-table/examples.js
@@ -58,40 +58,4 @@ const DefaultExample = () => (
   </BpkTable>
 );
 
-const AlternateExample = () => (
-  <BpkTable>
-    <BpkTableHead>
-      <BpkTableRow>
-        <BpkTableHeadCell alternate>Column 1</BpkTableHeadCell>
-        <BpkTableHeadCell alternate>Column 2</BpkTableHeadCell>
-        <BpkTableHeadCell alternate>Column 3</BpkTableHeadCell>
-      </BpkTableRow>
-    </BpkTableHead>
-    <BpkTableBody>
-      <BpkTableRow>
-        <BpkTableCell>Entry 1</BpkTableCell>
-        <BpkTableCell>Entry 2</BpkTableCell>
-        <BpkTableCell>Entry 3</BpkTableCell>
-      </BpkTableRow>
-      <BpkTableRow>
-        <BpkTableCell>Entry 4</BpkTableCell>
-        <BpkTableCell>Entry 5</BpkTableCell>
-        <BpkTableCell>Entry 6</BpkTableCell>
-      </BpkTableRow>
-      <BpkTableRow>
-        <BpkTableCell>Entry 7</BpkTableCell>
-        <BpkTableCell>Entry 8</BpkTableCell>
-        <BpkTableCell>Entry 9</BpkTableCell>
-      </BpkTableRow>
-    </BpkTableBody>
-  </BpkTable>
-);
-
-const MixedExample = () => (
-  <div>
-    <DefaultExample />
-    <AlternateExample />
-  </div>
-);
-
-export { DefaultExample, AlternateExample, MixedExample };
+export default DefaultExample;
