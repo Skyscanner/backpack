1619559687ae25a512633fcd79fcc97a2842716e
Ollie Curtis <8831547+olliecurtis@users.noreply.github.com>
2022-10-21 12:49:27 +0100

    Fix correct path for icons

--- gulpfile.js/bpk-component-icon/gulpfile.babel.js
+++ gulpfile.js/bpk-component-icon/gulpfile.babel.js
@@ -18,8 +18,7 @@
 
 import gulp from 'gulp';
 
-const ICONS_FOLDER_PATH =
-  '../../node_modules/@skyscanner/bpk-svgs/dist/js/icons';
+const ICONS_FOLDER_PATH = './node_modules/@skyscanner/bpk-svgs/dist/js/icons';
 
 gulp.task('copy', () =>
   gulp
