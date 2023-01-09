e30a982605df880ab5f29b1cf1e2bd952f817cb1
Ollie Curtis <8831547+olliecurtis@users.noreply.github.com>
2023-01-09 13:55:13 +0000

    [NO JIRA]: Removed autoprefixer and fixed browserlist warnings (#2683)

--- scripts/webpack/postCssPlugins.js
+++ scripts/webpack/postCssPlugins.js
@@ -16,13 +16,6 @@
  * limitations under the License.
  */
 
-const autoprefixer = require('autoprefixer');
 const postCssFlexbugsFixes = require('postcss-flexbugs-fixes');
 
-module.exports = () => [
-  postCssFlexbugsFixes,
-  autoprefixer({
-    browsers: ['>1%', 'last 4 versions', 'Firefox ESR', 'not ie < 9'],
-    flexbox: 'no-2009',
-  }),
-];
+module.exports = () => [postCssFlexbugsFixes];
