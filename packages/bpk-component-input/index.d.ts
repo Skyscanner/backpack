68c0a399f64a8918daf5374f4e941ba26f4a0a61
Ana Belciug <anabelciug@skyscanner.net>
2023-05-12 16:05:54 +0100

    Add license

--- packages/bpk-component-input/index.d.ts
+++ packages/bpk-component-input/index.d.ts
@@ -15,6 +15,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 import BpkInput from './src/BpkInput';
 import withOpenEvents from './src/withOpenEvents';
 import {
