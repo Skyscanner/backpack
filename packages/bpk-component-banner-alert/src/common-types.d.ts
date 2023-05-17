868dee9343f5a4c6c811dfce0ac07b9d65806e23
Ana Belciug <anabelciug@skyscanner.net>
2023-05-17 21:51:05 +0100

    Add dts files

---
+++ packages/bpk-component-banner-alert/src/common-types.d.ts
@@ -0,0 +1,44 @@
+/*
+ * Backpack - Skyscanner's Design System
+ *
+ * Copyright 2016 Skyscanner Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import type { FunctionComponent, ReactNode } from 'react';
+
+export declare const ALERT_TYPES: {
+  readonly PRIMARY: 'primary';
+  readonly SUCCESS: 'success';
+  readonly WARN: 'warn';
+  readonly ERROR: 'error';
+  readonly NEUTRAL: 'neutral';
+};
+export type AlertTypeValue = typeof ALERT_TYPES[keyof typeof ALERT_TYPES];
+export type CommonProps = {
+  type: AlertTypeValue;
+  message: ReactNode | string;
+  animateOnEnter?: boolean;
+  animateOnLeave?: boolean;
+  show?: boolean;
+  bannerClassName?: string | null;
+  icon?: FunctionComponent<any> | null;
+  [rest: string]: any;
+};
+export type OnExpandToggleHandler =
+  | ((expanded: boolean) => void)
+  | null
+  | undefined;
+export type OnDismissHandler = (() => void) | null | undefined;
+export type OnHideHandler = (() => void) | null | undefined;
