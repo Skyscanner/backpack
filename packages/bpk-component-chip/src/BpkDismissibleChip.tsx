895bc670a83b2275b0ce9d725fb9da8c8dd062af
Ana Belciug <anabelciug@skyscanner.net>
2023-05-26 15:42:29 +0100

    Remove TS import suppression

--- packages/bpk-component-chip/src/BpkDismissibleChip.tsx
+++ packages/bpk-component-chip/src/BpkDismissibleChip.tsx
@@ -22,7 +22,6 @@ been hard coded to have disabled={false}, selected and a trailing
 accessory view of a close icon.
 */
 
-// @ts-expect-error Untyped import. See `decisions/imports-ts-suppressions.md`.
 import { cssModules } from '../../bpk-react-utils';
 // @ts-expect-error Untyped import. See `decisions/imports-ts-suppressions.md`.
 import CloseCircleIconSm from '../../bpk-component-icon/sm/close-circle';
