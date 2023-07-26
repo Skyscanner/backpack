45273cb163eac08aadcbb2780dc7da33e829c0b4
Aleksandr Sannikov <aleksandr.sannikov@skyscanner.net>
2023-07-26 12:31:31 +0100

    Fixing pipeline

--- babel.config.js
+++ babel.config.js
@@ -24,25 +24,24 @@
  */
 
 module.exports = {
-  presets: [],
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        targets: {
+          node: 'current',
+        },
+        modules: false,
+      },
+    ],
+    // the option { runtime: 'automatic' } is passed to enable the new JSX transform where React is imported automatically
+    ['@babel/preset-react', { runtime: 'automatic' }],
+    // TODO: Remove this once we have migrated all flow types.
+    '@babel/preset-flow',
+    '@babel/preset-typescript',
+  ],
   env: {
     dev: {
-      presets: [
-        [
-          '@babel/preset-env',
-          {
-            targets: {
-              node: 'current',
-            },
-            modules: false,
-          },
-        ],
-        // the option { runtime: 'automatic' } is passed to enable the new JSX transform where React is imported automatically
-        ['@babel/preset-react', { runtime: 'automatic' }],
-        // TODO: Remove this once we have migrated all flow types.
-        '@babel/preset-flow',
-        '@babel/preset-typescript',
-      ],
       plugins: [
         [
           'module-resolver',
