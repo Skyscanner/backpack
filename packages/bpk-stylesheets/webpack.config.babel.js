71ec0d3d3457029951793460f5a70928f9af3b6c
Ollie Curtis <8831547+olliecurtis@users.noreply.github.com>
2023-08-03 16:55:01 +0100

    Bump mini-css-extract-plugin and wrapper-webpack-plugin to support webpack 5

--- packages/bpk-stylesheets/webpack.config.babel.js
+++ packages/bpk-stylesheets/webpack.config.babel.js
@@ -16,17 +16,37 @@
  * limitations under the License.
  */
 
-import MiniCssExtractPlugin from 'mini-css-extract-plugin';
-import WrapperPlugin from 'wrapper-webpack-plugin';
-
-import postCssPlugins from '../../scripts/webpack/postCssPlugins';
-
-import { blockComment as licenseHeader } from './license-header';
-
 const path = require('path');
 
+const WrapperPlugin = require('wrapper-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const postCssPlugins = require('../../scripts/webpack/postCssPlugins');
+
+const TEXT = `
+Backpack - Skyscanner's Design System
+
+Copyright 2016 Skyscanner Ltd
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+`;
+
+const licenseHeader = `/*
+${TEXT.replace(/^/gm, ' * ')}
+ */`;
+
 module.exports = {
-  mode: 'development',
+  mode: 'production',
   entry: {
     base: './index.js',
   },
