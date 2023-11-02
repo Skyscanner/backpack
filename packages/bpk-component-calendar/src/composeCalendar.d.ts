6b7c29efa29785b80d646c5ef872f87f89a59287
asim-majumder <asim.ahmed.majumder@gmail.com>
2023-11-02 14:17:17 +0000

    update dec files

--- packages/bpk-component-calendar/src/composeCalendar.d.ts
+++ packages/bpk-component-calendar/src/composeCalendar.d.ts
@@ -71,6 +71,7 @@ export type Props = {
     preventKeyboardFocus?: boolean;
     selectionConfiguration?: SelectionConfiguration;
     gridClassName?: string | null;
+    monthHeight?: number;
     /**
      * Key to be used to pick the desired weekDay format from the `daysOfWeek` object, for example: `nameAbbr` or `nameNarrow`.
      */
