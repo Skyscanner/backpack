96b9ea2e746dc05277a01e48aa95fd6e79465c38
Anthony Byledbal <anthony.byledbal@icloud.com>
2024-02-21 09:30:19 +0000

    prop

--- packages/bpk-component-datepicker/src/BpkDatepicker.tsx
+++ packages/bpk-component-datepicker/src/BpkDatepicker.tsx
@@ -307,7 +307,7 @@ class BpkDatepicker extends Component<Props, State> {
     // The following props are not used in render
     delete rest.onDateSelect;
     delete rest.onOpenChange;
-    // delete rest.isOpen;
+    delete rest.isOpen;
 
     console.log('state: ', this.state.propIsOpen)
     console.log('prop: ', this.props.isOpen)
