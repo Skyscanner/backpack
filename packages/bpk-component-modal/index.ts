5d6256f96577b139ae44b6edd615502db89795d6
Amelia Fitzpatrick <153074017+ameliajfitzpatrick@users.noreply.github.com>
2024-04-02 14:09:19 +0100

    DTO-5235 Add dark background flag for BpkModal and BpkModal v2 (#3323)
    * Colour changes for surface contrast applied to modal
    
    * Use existing token after discussion with Backpack
    
    * BpkModalV2
    
    * Changes to scss files
    
    * Updating type declaration files
    
    ---------
    
    Co-authored-by: Amelia Fitzpatrick <ameliafitzpatrick@skyscanner.net>

--- packages/bpk-component-modal/index.ts
+++ packages/bpk-component-modal/index.ts
@@ -19,6 +19,7 @@
 import { BpkModalV2 } from './src/BpkModalV2/BpkModal';
 import themeAttributes from './src/themeAttributes';
 import BpkModal from './src/BpkModal';
+import { MODAL_STYLING } from './src/BpkModalInner';
 // @ts-expect-error Untyped import. See `decisions/imports-ts-suppressions.md`.
 import { propTypes, defaultProps } from './src/legacy-prop-types';
 
@@ -27,4 +28,4 @@ import type { Props } from './src/BpkModal';
 export type BpkModalProps = Props;
 
 export default BpkModal;
-export { propTypes, defaultProps, themeAttributes, BpkModalV2 };
+export { propTypes, defaultProps, themeAttributes, BpkModalV2, MODAL_STYLING };
