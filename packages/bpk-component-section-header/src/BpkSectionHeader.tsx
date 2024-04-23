af5e3120425148a7528de514f18482665d68a90a
Matthew Robertson <matthew.robertson@skyscanner.net>
2024-04-23 14:23:20 +0100

    [LOOM-1313][BpkSectionHeader] Class2 Fixes (#3380)
    * trial with spans
    
    * set textStyle
    
    * set as h2 tag
    
    ---------
    
    Co-authored-by: metalix2 <matthewr@skyscanner.net>

--- packages/bpk-component-section-header/src/BpkSectionHeader.tsx
+++ packages/bpk-component-section-header/src/BpkSectionHeader.tsx
@@ -52,19 +52,12 @@ const BpkSectionHeader = ({
         `bpk-section-header__title-description--${type}`,
       )}
     >
-      <BpkText
-        tagName="h2"
-        // TODO: className to be removed
-        // eslint-disable-next-line @skyscanner/rules/forbid-component-props
-        className={getClassName('bpk-section-header__title')}
-      >
-        {title}
-      </BpkText>
-      {description && <BpkText 
-        // TODO: className to be removed
-        // eslint-disable-next-line @skyscanner/rules/forbid-component-props
-        className={getClassName('bpk-section-header__description')}>{description}
-      </BpkText>}
+      <h2 className={getClassName('bpk-section-header__title')}>{title}</h2>
+      {description && (
+        <span className={getClassName('bpk-section-header__description')}>
+          <BpkText>{description}</BpkText>
+        </span>
+      )}
     </div>
     {button}
   </div>
