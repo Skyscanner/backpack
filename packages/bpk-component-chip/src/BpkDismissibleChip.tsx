289724a6727f71fe15fe84bded6935735d865bcb
metalix2 <matthewr@skyscanner.net>
2024-04-24 14:58:24 +0100

    ignore-ts error

--- packages/bpk-component-chip/src/BpkDismissibleChip.tsx
+++ packages/bpk-component-chip/src/BpkDismissibleChip.tsx
@@ -21,6 +21,7 @@ The dismissible chip component is just a selectable chip that's
 been hard coded to have disabled={false}, selected and a trailing
 accessory view of a close icon.
 */
+// @ts-expect-error Untyped import. See `decisions/imports-ts-suppressions.md`.
 import { chipColors } from '@skyscanner/bpk-foundations-web/tokens/base.es6';
 
 // @ts-expect-error Untyped import. See `decisions/imports-ts-suppressions.md`.
