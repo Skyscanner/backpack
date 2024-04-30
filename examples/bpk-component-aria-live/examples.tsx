e3375459cf66d0067638c96659f05635e3cae52a
Mungo Dewar <mungodewar@skyscanner.net>
2024-04-30 14:47:22 +0100

    remove unrequired flow comments

--- examples/bpk-component-aria-live/examples.tsx
+++ examples/bpk-component-aria-live/examples.tsx
@@ -15,7 +15,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-/* @flow strict */
 
 import { Component } from 'react';
 import type { ChangeEvent, ReactElement } from 'react';
