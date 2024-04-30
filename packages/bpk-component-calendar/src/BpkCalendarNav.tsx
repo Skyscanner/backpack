e9de4766d4d6e08e4057a8e98330fb416bf9dfef
metalix2 <matthewr@skyscanner.net>
2024-04-30 15:04:03 +0100

    removed ts-expect-error

--- packages/bpk-component-calendar/src/BpkCalendarNav.tsx
+++ packages/bpk-component-calendar/src/BpkCalendarNav.tsx
@@ -20,7 +20,6 @@ import type { ChangeEvent, MouseEvent } from 'react';
 
 import {
   textColors,
-  // @ts-expect-error Untyped import. See `decisions/imports-ts-suppressions.md`.
 } from '@skyscanner/bpk-foundations-web/tokens/base.es6';
 
 import BpkAriaLive from '../../bpk-component-aria-live';
