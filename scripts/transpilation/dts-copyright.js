cfbd28b1ce9c59e400ad78127280b9b738f096fb
Mungo Dewar <mungodewar@skyscanner.net>
2024-05-03 09:39:52 +0100

    Add to transpilation pipeline - Add .d.ts file output to dist - Create script to add copyright header to each .d.ts file - Create additional tsconfig for outputing .d.ts files

---
+++ scripts/transpilation/dts-copyright.js
@@ -0,0 +1,54 @@
+/*
+ * Backpack - Skyscanner's Design System
+ *
+ * Copyright 2016 Skyscanner Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+const fs = require('fs');
+const path = require('path');
+
+const glob = require('glob');
+
+const HEADER = `
+/*
+ * Backpack - Skyscanner's Design System
+ *
+ * Copyright 2016 Skyscanner Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+`
+const files = glob.sync(path.resolve(`${path.dirname(__dirname)}/../dist/**/*.d.ts`), {
+  ignore: ['**/node_modules/**']
+});
+
+for (const file of files) {
+  let contents = fs.readFileSync(file, 'utf8');
+  if (!/Copyright \d+ Skyscanner/.test(contents)) {
+    contents = `${HEADER  }\n${  contents}`;
+    fs.writeFileSync(file, contents);
+  }
+}
