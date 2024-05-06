d5db7114b77e805d02fea0e041e543e2b9fea88e
metalix2 <matthewr@skyscanner.net>
2024-05-06 17:28:45 +0100

    remove unused imports

--- examples/bpk-component-modal/stories.tsx
+++ examples/bpk-component-modal/stories.tsx
@@ -15,7 +15,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 import BpkModal from '../../packages/bpk-component-modal/src/BpkModal';
 
 import {
@@ -60,42 +59,51 @@ export const NoPadding = NoPaddingExample;
 export const WithAccessoryView = WithAccessoryViewExample;
 export const Contrast = ContrastExample;
 
-const visualWrapper = (id: string) => (
-  <div style={{'height': '640px', width: '100%'}}>
+// Due to how iframes work we can pass a local url to load the stories above.
+// Attempted to use a Custom Iframe component with a react portal and ref to
+// render components but it didn't have the desired effect.
+const visualWrapper = (id: string, zoomEnabled: boolean = false) => (
+  <div style={{ height: '640px', width: '100%' }}>
     <iframe
       title={`Embedded Storybook ${id}`}
-      src={`/iframe.html?args=&id=${id}&viewMode=story&isOpen=true&padded=false`}
+      src={`/iframe.html?id=${id}&viewMode=story&args=zoomEnabled:${zoomEnabled}`}
       loading="lazy"
       aria-label="Embedded Storybook"
       referrerPolicy="origin"
-      style={{'height': '100%', width: '100%', border: 0}}
+      style={{ height: '100%', width: '100%', border: 0 }}
     />
   </div>
 );
+
 export const VisualTestDefault = {
-  render: () => <>
-    {visualWrapper("bpk-component-modal--default")}
-    {visualWrapper("bpk-component-modal--contrast")}
-    {visualWrapper("bpk-component-modal--full-screen")}
-    {visualWrapper("bpk-component-modal--with-accessory-view")}
-    {visualWrapper("bpk-component-modal--no-padding")}
-  </>,
+  render: () => (
+    <>
+      {visualWrapper('bpk-component-modal--default')}
+      {visualWrapper('bpk-component-modal--contrast')}
+      {visualWrapper('bpk-component-modal--full-screen')}
+      {visualWrapper('bpk-component-modal--long-title')}
+      {visualWrapper('bpk-component-modal--with-accessory-view')}
+      {visualWrapper('bpk-component-modal--no-padding')}
+      {visualWrapper('bpk-component-modal--no-header')}
+    </>
+  ),
   parameters: {
     layout: 'fullscreen',
   },
 };
 export const VisualTestDefaultWithZoom = {
-  render: () => <>
-  {visualWrapper("bpk-component-modal--default")}
-  {visualWrapper("bpk-component-modal--contrast")}
-  {visualWrapper("bpk-component-modal--full-screen")}
-  {visualWrapper("bpk-component-modal--with-accessory-view")}
-  {visualWrapper("bpk-component-modal--no-padding")}
-</>,
-parameters: {
-  layout: 'fullscreen',
-},
-  args: {
-    zoomEnabled: true,
+  render: () => (
+    <>
+      {visualWrapper('bpk-component-modal--default', true)}
+      {visualWrapper('bpk-component-modal--contrast', true)}
+      {visualWrapper('bpk-component-modal--full-screen', true)}
+      {visualWrapper('bpk-component-modal--long-title', true)}
+      {visualWrapper('bpk-component-modal--with-accessory-view', true)}
+      {visualWrapper('bpk-component-modal--no-padding', true)}
+      {visualWrapper('bpk-component-modal--no-header', true)}
+    </>
+  ),
+  parameters: {
+    layout: 'fullscreen',
   },
 };
