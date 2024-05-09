99d5c8861ecff83ec37c7a34162ac001be561208
Ollie Curtis <8831547+olliecurtis@users.noreply.github.com>
2024-05-09 10:44:45 +0100

    [ARGG-1063][BpkPopover]: Migrate to floating UI library (#3390)
    [ARGG-1063][BpkPopover]: Migrate to floating UI library (#3390)
    
    * Migrate BpkPopover to floating UI
    * Adding type definition files
    * Updating datepicker usage of popover
    * Addressing feedback
    * remove unused type directive
    * Add migration guide
    * Fix clipping/shadow/gap of arrow and popover
    * Update Placement type to allow for floating UI expected

--- packages/bpk-component-popover/src/constants.tsx
+++ packages/bpk-component-popover/src/constants.tsx
@@ -16,17 +16,6 @@
  * limitations under the License.
  */
 
-/* @flow strict */
+export const ARROW_ID = 'js-bpk-popover-arrow';
 
-import themeAttributes from './themeAttributes';
-
-describe('themeAttributes', () => {
-  it('should export the correct theme attributes', () => {
-    expect(themeAttributes).toEqual([
-      'linkColor',
-      'linkHoverColor',
-      'linkActiveColor',
-      'linkVisitedColor',
-    ]);
-  });
-});
+export default { ARROW_ID };
