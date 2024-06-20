25079845f1d214a01f9d7f07feea19f1e755fe5e
Jenna Mclelland <104928354+Jennahowieson@users.noreply.github.com>
2024-06-20 17:18:54 +0100

    revert commit that removed state

--- packages/bpk-component-segmented-control/src/BpkSegmentedControl.tsx
+++ packages/bpk-component-segmented-control/src/BpkSegmentedControl.tsx
@@ -51,8 +51,10 @@ const BpkSegmentedControl = ({
   shadow = false,
   type = SEGMENT_TYPES.CanvasDefault,
 }: Props) => {
+  const [selectedButton, setSelectedButton] = useState(selectedIndex);
   const handleButtonClick = (id: number) => {
-    if (id !== selectedIndex) {
+    if (id !== selectedButton) {
+      setSelectedButton(id);
       onItemClick(id);
     }
   };
@@ -64,8 +66,8 @@ const BpkSegmentedControl = ({
   return (
     <div role="radiogroup" className={containerStyling}>
       {buttonContents.map((content, index) => {
-        const isSelected = index === selectedIndex;
-        const rightOfOption = index === selectedIndex + 1;
+        const isSelected = index === selectedButton;
+        const rightOfOption = index === selectedButton + 1;
         const buttonStyling = getClassName(
           'bpk-segmented-control',
           `bpk-segmented-control--${type}`,
