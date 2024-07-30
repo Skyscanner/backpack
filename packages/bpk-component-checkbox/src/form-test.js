095809562ef559cdb24eeb2420529f54c97f7856
metalix2 <matthewr@skyscanner.net>
2024-07-30 16:49:46 +0100

    Form validation test for form components

---
+++ packages/bpk-component-checkbox/src/form-test.js
@@ -0,0 +1,88 @@
+/*
+ * Backpack - Skyscanner's Design System
+ *
+ * Copyright 2016 Skyscanner Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { useState, useEffect } from 'react';
+
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
+import BpkCheckbox from './BpkCheckbox';
+
+describe('BpkCheckbox form test', () => {
+  it('should work as a form component in a form', async () => {
+    const Wrap = () => {
+      // state is required to force react to update and re-render the component.
+      const [isChecked, setIsChecked] = useState(false);
+      return (
+        <form data-testid="form">
+          <BpkCheckbox
+            type="checkbox"
+            name="checkbox"
+            checked={isChecked}
+            onChange={() => setIsChecked(!isChecked)}
+            data-testid="mycheckbox"
+          />
+          <button type="submit">Submit</button>
+        </form>
+      );
+    };
+    render(<Wrap />);
+
+    const checkbox = screen.getByTestId('mycheckbox');
+    expect(checkbox).not.toBeChecked();
+
+    await userEvent.click(checkbox);
+    expect(checkbox).toBeChecked();
+
+    const formData = new FormData(screen.getByTestId('form'));
+    expect(Object.fromEntries(formData.entries())).toEqual({ checkbox: 'on' });
+  });
+
+  it('should emit change event when toggled', async () => {
+    const formValidation = jest.fn();
+    const Wrap = () => {
+      // state is required to force react to update and re-render the component.
+      const [isChecked, setIsChecked] = useState(false);
+      useEffect(() => {
+        document.addEventListener('change', formValidation);
+      }, []);
+      return (
+        <form data-testid="form">
+          <BpkCheckbox
+            type="checkbox"
+            name="checkbox"
+            checked={isChecked}
+            onChange={() => setIsChecked(!isChecked)}
+            data-testid="mycheckbox"
+          />
+          <button type="submit">Submit</button>
+        </form>
+      );
+    };
+    render(<Wrap />);
+
+    const checkbox = screen.getByTestId('mycheckbox');
+    expect(checkbox).not.toBeChecked();
+    expect(formValidation).not.toHaveBeenCalled();
+
+    await userEvent.click(checkbox);
+    expect(checkbox).toBeChecked();
+
+    expect(formValidation).toHaveBeenCalledTimes(1);
+  });
+});
