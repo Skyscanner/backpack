5169c232920cae8480d494cbcaf80c1dd8a5fac6
Tao Fang <85286316+taofang2021@users.noreply.github.com>
2024-08-02 16:53:50 +0800

    [CYB-2251][Snippet] Add new Snippet component (#3531)
    * [CYB-2251][Snippet] Add new Snippet component
    
    * add vertical style for web
    
    * Add radius style and update stories
    
    * fix comments
    
    * replace img with bpkimage
    
    * update
    
    * update as comments
    
    * update
    
    * update example

---
+++ packages/bpk-component-snippet/src/BpkSnippet-test.tsx
@@ -0,0 +1,64 @@
+/*
+ * Backpack - Skyscanner's Design System
+ *
+ * Copyright 2016 Skyscanner Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { render, screen } from '@testing-library/react';
+
+import BpkSnippet, { DESKTOP_LAYOUT } from './BpkSnippet';
+
+const props = {
+  src: 'https://content.skyscnr.com/m/f427e62297cce49/original/edinburgh-view-from-calton-hill.jpg',
+  altText: 'image description',
+  headline: 'Title of the section',
+  subheading: 'Subheading',
+  bodyText: 'Lorem ipsum dolor sit amet consectetur.',
+  buttonText: 'Call to Action',
+  onClick: () => window.open('https://www.skyscanner.net/flights', '_blank'),
+};
+
+describe('BpkSnippet', () => {
+  it('should render desktop correctly', () => {
+    const { container } = render(<BpkSnippet {...props} />);
+    expect(container.querySelectorAll('.bpk-snippet--image').length).toBe(1);
+    expect(screen.getByText('Title of the section')).toBeVisible();
+    expect(screen.getByText('Subheading')).toBeInTheDocument();
+    expect(
+      screen.getByText('Lorem ipsum dolor sit amet consectetur.'),
+    ).toBeInTheDocument();
+    expect(screen.getByText('Call to Action')).toBeInTheDocument();
+  });
+
+  it('should render desktop reverse correctly', () => {
+    const { container } = render(
+      <BpkSnippet {...props} desktopLayout={DESKTOP_LAYOUT.imageRight} />,
+    );
+    expect(container.querySelectorAll('.bpk-snippet--image').length).toBe(1);
+    expect(container.querySelectorAll('.bpk-snippet--row-reverse').length).toBe(
+      1,
+    );
+  });
+
+  it('should render desktop vertical correctly', () => {
+    const { container } = render(
+      <BpkSnippet {...props} desktopLayout={DESKTOP_LAYOUT.vertical} />,
+    );
+    expect(container.querySelectorAll('.bpk-snippet--image').length).toBe(1);
+    expect(container.querySelectorAll('.bpk-snippet--vertical').length).toBe(
+      1,
+    );
+  });
+});
---
+++ packages/bpk-component-snippet/src/accessibility-test.tsx
@@ -0,0 +1,47 @@
+/*
+ * Backpack - Skyscanner's Design System
+ *
+ * Copyright 2016 Skyscanner Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { render } from '@testing-library/react';
+import { axe } from 'jest-axe';
+
+import { BUTTON_TYPES } from '../../bpk-component-button';
+import { TEXT_STYLES } from '../../bpk-component-text/src/BpkText';
+
+import BpkSnippet from './BpkSnippet';
+
+describe('BpkJourneyArrow accessibility tests', () => {
+  it('should not have programmatically-detectable accessibility issues', async () => {
+    const { container } = render(
+      <BpkSnippet
+        src="https://content.skyscnr.com/m/f427e62297cce49/original/edinburgh-view-from-calton-hill.jpg"
+        altText="image description"
+        headline="Title of the section"
+        subheading="Subheading"
+        bodyText="Lorem ipsum dolor sit amet consectetur. Tristique at pharetra tincidunt elementum vulputate varius sit euismod hac. Dignissim hendrerit enim eros nisi diam. Elit arcu mattis cum in id varius vitae augue neque. Quisque in semper malesuada lacus ut etiam elementum."
+        bodyStyle={TEXT_STYLES.bodyDefault}
+        buttonStyle={BUTTON_TYPES.primary}
+        headlineStyle={TEXT_STYLES.hero5}
+        desktopLayout="imageLeft"
+        imageOrientation="landscape"
+        buttonText="Call to Action"
+      />,
+    );
+    const results = await axe(container);
+    expect(results).toHaveNoViolations();
+  });
+});
