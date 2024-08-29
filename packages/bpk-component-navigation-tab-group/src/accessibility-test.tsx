d33e3c04711b64b1c6115f4cb47d49d508e170d1
Kerrie Wu <kerrie.wu@skyscanner.net>
2024-08-29 13:55:46 +0800

    add Navigation-tab-group

---
+++ packages/bpk-component-navigation-tab-group/src/accessibility-test.tsx
@@ -0,0 +1,62 @@
+/*
+ * Backpack - Skyscanner's Design System
+ *
+ * Copyright 2016 Skyscanner Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { render } from '@testing-library/react';
+import { axe } from 'jest-axe';
+
+import BpkNavigationTabGroup from './BpkNavigationTabGroup';
+
+import type { Props } from './BpkNavigationTabGroup';
+
+const tabs: Props['tabs'] = [
+  { text: 'Flights', href: '/' },
+  { text: 'Hotels', href: '/hotel' },
+  { text: 'Car hire', href: '/carhire' },
+];
+
+const tabsNoHref: Props['tabs'] = [
+  { text: 'Flights'},
+  { text: 'Hotels'},
+  { text: 'Car hire'},
+];
+
+describe('BpkNavigationTabGroup accessibility tests', () => {
+  it('should not have programmatically-detectable accessibility issues', async () => {
+    const { container } = render(
+      <BpkNavigationTabGroup
+        tabs={tabs}
+        onItemClick={() => {}}
+        selectedIndex={0}
+      />,
+    );
+    const results = await axe(container);
+    expect(results).toHaveNoViolations();
+  });
+
+  it('should not have programmatically-detectable accessibility issues without href', async () => {
+    const { container } = render(
+      <BpkNavigationTabGroup
+        tabs={tabsNoHref}
+        onItemClick={() => {}}
+        selectedIndex={0}
+      />,
+    );
+    const results = await axe(container);
+    expect(results).toHaveNoViolations();
+  });
+});
