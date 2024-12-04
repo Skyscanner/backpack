e61ff6722d313710079c3bb3a5880b9d17358b37
sarak-works <sarak.works@gmail.com>
2024-12-04 15:28:36 +0000

    Wrap BpkCalendar format date methods with memoize

--- packages/bpk-component-calendar/src/composeCalendar.tsx
+++ packages/bpk-component-calendar/src/composeCalendar.tsx
@@ -18,6 +18,8 @@
 
 import type { ComponentType } from 'react';
 
+import { memoize } from 'lodash';
+
 import { cssModules } from '../../bpk-react-utils';
 
 import { CALENDAR_SELECTION_TYPE } from './custom-proptypes';
@@ -184,7 +186,7 @@ const composeCalendar = (
         {Nav && (
           <Nav
             changeMonthLabel={changeMonthLabel}
-            formatMonth={formatMonth}
+            formatMonth={memoize(formatMonth)}
             id={`${id}__bpk_calendar_nav`}
             maxDate={maxDate}
             minDate={minDate}
@@ -210,8 +212,8 @@ const composeCalendar = (
           DateComponent={CalendarDate}
           dateModifiers={dateModifiers}
           daysOfWeek={daysOfWeek}
-          formatDateFull={formatDateFull}
-          formatMonth={formatMonth}
+          formatDateFull={memoize(formatDateFull)}
+          formatMonth={memoize(formatMonth)}
           month={month}
           onDateClick={onDateClick}
           onDateKeyDown={onDateKeyDown}
