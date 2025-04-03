b24dbd14b71beac72f154e632dfb105f9abdf0a5
Steven-Zeng <Steven.Zeng@skyscanner.net>
2025-04-03 17:31:20 +0800

    remove flow strict

--- packages/bpk-component-page-indicator/src/NavButton.tsx
+++ packages/bpk-component-page-indicator/src/NavButton.tsx
@@ -15,7 +15,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-/* @flow strict */
 
 import {BUTTON_TYPES, BpkButtonV2} from '../../bpk-component-button';
 import { withButtonAlignment, withRtlSupport } from '../../bpk-component-icon';
