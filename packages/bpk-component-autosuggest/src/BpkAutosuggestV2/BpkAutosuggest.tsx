2800f1801f6d8376c2b3b3b335cb04023163e975
Kerrie Wu <kerrie.wu@skyscanner.net>
2025-06-04 15:58:44 +0800

    add danger

--- packages/bpk-component-autosuggest/src/BpkAutosuggestV2/BpkAutosuggest.tsx
+++ packages/bpk-component-autosuggest/src/BpkAutosuggestV2/BpkAutosuggest.tsx
@@ -1,3 +1,21 @@
+/*
+ * Backpack - Skyscanner's Design System
+ *
+ * Copyright 2016 Skyscanner Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import { useEffect, forwardRef } from 'react';
 import type {
   KeyboardEvent,
@@ -9,7 +27,7 @@ import type {
 
 import { useCombobox } from 'downshift';
 
-import BpkInput, { INPUT_TYPES } from '../../../bpk-component-input';
+import { INPUT_TYPES } from '../../../bpk-component-input';
 import { cssModules } from '../../../bpk-react-utils';
 
 import type {
