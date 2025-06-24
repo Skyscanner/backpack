463d50f8a0708c1b3ebbad5a9c3f8c629f9d3997
Steven-Zeng <Steven.Zeng@skyscanner.net>
2025-06-24 14:33:20 +0800

    [Part 2][Version 5.4.1]Small fix for onExpandToggle

--- packages/bpk-component-card-list/src/BpkExpand/ExpandAccessoryContent.tsx
+++ packages/bpk-component-card-list/src/BpkExpand/ExpandAccessoryContent.tsx
@@ -37,7 +37,7 @@ const ExpandAccessoryContent = ({
     <BpkButtonV2
       data-testid="bpk-card-list__accessory-expand-button"
       type={BUTTON_TYPES.link}
-      onClick={() => onExpandToggle()}
+      onClick={onExpandToggle}
       aria-expanded={!collapsed}
     >
       {children}
