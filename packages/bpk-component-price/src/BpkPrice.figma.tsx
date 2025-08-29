11b35d6796608135bef1b77d9241d5dc11bf1d07
Gert-Jan Vercauteren <gert-jan.vercauteren@skyscanner.net>
2025-08-29 10:12:20 +0800

    feat: add price mapping

---
+++ packages/bpk-component-price/src/BpkPrice.figma.tsx
@@ -0,0 +1,27 @@
+import React from "react"
+import BpkPrice from "./BpkPrice"
+import { SIZES, ALIGNS } from './common-types';
+import figma from "@figma/code-connect"
+
+figma.connect(
+  BpkPrice,
+  "https://www.figma.com/design/irZ3YBx8vOm16ICkAr7mB3/Backpack-Components?node-id=16470%3A17021",
+  {
+    props: {
+      price: figma.string("Price"),
+      size: figma.enum("Size", {
+        "X-Small": SIZES.xsmall,
+        Small: SIZES.small,
+        Large: SIZES.large,
+      }),
+      align: figma.enum("Alignment", {
+        Left: ALIGNS.left,
+        Right: ALIGNS.right,
+      }),
+      trailingText: figma.string('Trailing text'),
+    },
+    example: ({ price, size, align, trailingText }) => (
+      <BpkPrice price={price} size={size} align={align} trailingText={trailingText} />
+    ),
+  },
+)
