db7cf767d21a746104f6ccf5ffbb06d44416ec69
Flora Cheng <flora.cheng@skyscanner.net>
2025-09-23 15:44:16 +0800

    connect fieldset to figma

--- packages/bpk-component-fieldset/src/BpkFieldset.figma.tsx
+++ packages/bpk-component-fieldset/src/BpkFieldset.figma.tsx
@@ -23,86 +23,90 @@ import BpkInput, { INPUT_TYPES } from "../../bpk-component-input";
 import BpkSelect from "../../bpk-component-select";
 import BpkTextarea from "../../bpk-component-textarea";
 
-import BpkFieldset from "./BpkFieldset"
+import BpkFieldset from "./BpkFieldset";
 
+const figmaUrl = "https://www.figma.com/design/irZ3YBx8vOm16ICkAr7mB3/Backpack-Components?node-id=30459%3A44425";
 
-figma.connect(
-  BpkFieldset,
-  "https://www.figma.com/design/irZ3YBx8vOm16ICkAr7mB3/Backpack-Components?node-id=30459%3A44425",
-  {
-    props: {
-      error: figma.boolean('Error'),
-      type: figma.enum('Type', {
-        Input: 'input',
-        Select: 'select',
-        'Text area': 'text-area',
-        'Radio group': 'radio-group',
-      }),
-    },
-    example: (props) => {
-      let child;
-      switch (props.type) {
-        case 'input':
-          child = (
-            <BpkInput
-              id="example-input"
-              name="example-input"
-              value=""
-              type={INPUT_TYPES.text}
-            />
-          );
-          break;
-        case 'select':
-          child = (
-            <BpkSelect
-              id="example-select"
-              name="example-select"
-              value=""
-            >
-              <option value="">Select</option>
-              <option value="option1">Option 1</option>
-              <option value="option2">Option 2</option>
-            </BpkSelect>
-          );
-          break;
-        case 'text-area':
-          child = (
-            <BpkTextarea
-              id="textarea"
-              name="textarea"
-              value=""
-            />
-          );
-          break;
-        case 'radio-group':
-          child = (
-            <div id="example-radio-group">
-              <BpkCheckbox name="group" label="Radio 1" checked={false} />
-              <BpkCheckbox name="group" label="Radio 2" checked={false} />
-            </div>
-          );
-          break;
-        default:
-          child = (
-            <BpkInput
-              id="example-input"
-              name="example-input"
-              value=""
-              type={INPUT_TYPES.text}
-            />
-          );
-      }
-      // Provide required props for BpkFieldset
-      return (
-        <BpkFieldset
-          label="Example Fieldset"
-          required={false}
-          disabled={false}
-          valid={!props.error}
-        >
-          {child}
-        </BpkFieldset>
-      );
-    },
+// Input variant
+figma.connect(BpkFieldset, figmaUrl, {
+  variant: { Type: "Input" },
+  props: {
+    error: figma.boolean("Error"),
   },
-)
+  example: (props) => (
+    <BpkFieldset
+      label="Example Fieldset"
+      required={false}
+      disabled={false}
+      valid={!props.error}
+    >
+      <BpkInput
+        id="example-input"
+        name="example-input"
+        value=""
+        type={INPUT_TYPES.text}
+      />
+    </BpkFieldset>
+  ),
+});
+
+// Select variant
+figma.connect(BpkFieldset, figmaUrl, {
+  variant: { Type: "Select" },
+  props: {
+    error: figma.boolean("Error"),
+  },
+  example: (props) => (
+    <BpkFieldset
+      label="Example Fieldset"
+      required={false}
+      disabled={false}
+      valid={!props.error}
+    >
+      <BpkSelect id="example-select" name="example-select" value="">
+        <option value="">Select</option>
+        <option value="option1">Option 1</option>
+        <option value="option2">Option 2</option>
+      </BpkSelect>
+    </BpkFieldset>
+  ),
+});
+
+// Text area variant
+figma.connect(BpkFieldset, figmaUrl, {
+  variant: { Type: "Text area" },
+  props: {
+    error: figma.boolean("Error"),
+  },
+  example: (props) => (
+    <BpkFieldset
+      label="Example Fieldset"
+      required={false}
+      disabled={false}
+      valid={!props.error}
+    >
+      <BpkTextarea id="textarea" name="textarea" value="" />
+    </BpkFieldset>
+  ),
+});
+
+// Radio group variant
+figma.connect(BpkFieldset, figmaUrl, {
+  variant: { Type: "Radio group" },
+  props: {
+    error: figma.boolean("Error"),
+  },
+  example: (props) => (
+    <BpkFieldset
+      label="Example Fieldset"
+      required={false}
+      disabled={false}
+      valid={!props.error}
+    >
+      <div id="example-radio-group">
+        <BpkCheckbox name="group" label="Radio 1" checked={false} />
+        <BpkCheckbox name="group" label="Radio 2" checked={false} />
+      </div>
+    </BpkFieldset>
+  ),
+});
