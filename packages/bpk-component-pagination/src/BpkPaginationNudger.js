dd1ffcbe591e618f7fe4cf84ebe0d9fd103995c8
Ezreal Yang <supremeyh@126.com>
2025-12-12 09:15:37 +0800

    update BpkPaginationNudger to iconOnly

--- packages/bpk-component-pagination/src/BpkPaginationNudger.js
+++ packages/bpk-component-pagination/src/BpkPaginationNudger.js
@@ -42,7 +42,7 @@ const BpkPaginationNudger = (props) => {
         type={BUTTON_TYPES.link}
         onClick={onNudge}
         disabled={disabled}
-        implicit
+        iconOnly
       >
         {nudgerIcon(forward)}
         <span className={getClassName('bpk-pagination-nudger__text--hidden')}>
