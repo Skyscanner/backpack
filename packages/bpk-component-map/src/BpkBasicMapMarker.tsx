b32b4055266f4b49aef7c761ba281e7607451108
Gert-Jan Vercauteren <gert-jan.vercauteren@skyscanner.net>
2026-01-30 11:03:35 +0800

    feat: Add data component attributes - Specialized
    Phase 11/11 of data-backpack-ds-component migration
    
    This phase adds data-backpack-ds-component attributes to specialized components.

--- packages/bpk-component-map/src/BpkBasicMapMarker.tsx
+++ packages/bpk-component-map/src/BpkBasicMapMarker.tsx
@@ -19,10 +19,13 @@
 import PropTypes from 'prop-types';
 import type { ReactNode } from 'react';
 
+import { getDataComponentAttribute } from '../../bpk-react-utils';
+
 // @ts-expect-error Untyped import. See `decisions/imports-ts-suppressions.md`.
 import BpkOverlayView from './BpkOverlayView';
 // @ts-expect-error Untyped import. See `decisions/imports-ts-suppressions.md`.
 import { LatLongPropType, type LatLong } from './common-types';
+
 
 type Props = {
   children: ReactNode,
@@ -41,7 +44,7 @@ const BpkBasicMapMarker = (props: Props) => {
     <BpkOverlayView
       position={position}
       getPixelPositionOffset={getPixelPositionOffset}
-      {...rest}
+      {...getDataComponentAttribute('BasicMapMarker')} {...rest}
     >
       {children}
     </BpkOverlayView>
