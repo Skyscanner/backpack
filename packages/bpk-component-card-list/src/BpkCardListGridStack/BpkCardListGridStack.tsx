03b852ff7dbbdadd7fc52cdebbf42b6e6e52c3af
Gert-Jan Vercauteren <gert-jan.vercauteren@skyscanner.net>
2026-01-30 11:03:00 +0800

    feat: Add data component attributes - Containers & Layout
    Phase 6/11 of data-backpack-ds-component migration
    
    This phase adds data-backpack-ds-component attributes to containers & layout components.

--- packages/bpk-component-card-list/src/BpkCardListGridStack/BpkCardListGridStack.tsx
+++ packages/bpk-component-card-list/src/BpkCardListGridStack/BpkCardListGridStack.tsx
@@ -20,7 +20,7 @@ import type { CSSProperties } from 'react';
 import { Children, useState } from 'react';
 
 import BpkButton from '../../../bpk-component-button';
-import { cssModules } from '../../../bpk-react-utils';
+import { cssModules, getDataComponentAttribute } from '../../../bpk-react-utils';
 import ExpandAccessoryContent from '../BpkExpand/ExpandAccessoryContent';
 import { ACCESSORY_DESKTOP_TYPES } from '../common-types';
 
@@ -77,7 +77,7 @@ const BpkCardListGridStack = (props: CardListGridStackProps) => {
 
   const buttonAccessoryContent = (
     <div
-      className={getClassName('bpk-card-list-grid-stack__accessory__button')}
+      className={getClassName('bpk-card-list-grid-stack__accessory__button')} {...getDataComponentAttribute('CardListGridStack')}
     >
       <BpkButton
         data-testid="bpk-card-list__accessory-button"
