bcdf936823cc1c45a87e10ea43f061f4bae874c3
Gert-Jan Vercauteren <gert-jan.vercauteren@skyscanner.net>
2026-06-26 17:25:04 +0800

    [bpk-react-utils] Migrate deprecated.js to TypeScript
    Migrates bpk-react-utils from JavaScript to TypeScript as described in #4753.
    
    - Replaced prop-types with TypeScript interfaces
    - Converted defaultProps to ES6 destructure defaults
    - Renamed .js source/test/story files to .tsx/.ts
    - Removed Flow type annotations
    - Preserved Apache 2.0 license headers
    
    Closes #4753

---
+++ packages/backpack-web/src/bpk-react-utils/src/deprecated.ts
@@ -0,0 +1,46 @@
+/*
+ * Backpack - Skyscanner's Design System
+ *
+ * Copyright 2016 Skyscanner Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// TODO: Remove this file once all BPK components have been migrated to TS
+
+type PropType = (
+  props: { [key: string]: any },
+  propName: string,
+  componentName: string,
+  ...rest: any[]
+) => any;
+
+const deprecated = (
+  propType: PropType,
+  alternativeSuggestion: string,
+): PropType =>
+  (
+    props: { [key: string]: any },
+    propName: string,
+    componentName: string,
+    ...rest: any[]
+  ) => {
+    if (props[propName] != null) {
+      const message = `Warning: "${propName}" property of "${componentName}" has been deprecated. ${alternativeSuggestion}`;
+      // eslint-disable-next-line no-console
+      console.warn(message);
+    }
+    return propType(props, propName, componentName, ...rest);
+  };
+
+export default deprecated;
