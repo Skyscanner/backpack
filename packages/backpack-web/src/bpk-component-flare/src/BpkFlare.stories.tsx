aaa5ad09fdb5bd9fd7776e5b5cf08d35e6ad1853
Gert-Jan Vercauteren <gert-jan.vercauteren@skyscanner.net>
2026-07-22 18:58:22 +0800

    chore: reset non-CSS files to main

--- packages/backpack-web/src/bpk-component-flare/src/BpkFlare.stories.tsx
+++ packages/backpack-web/src/bpk-component-flare/src/BpkFlare.stories.tsx
@@ -18,8 +18,6 @@
 
 import { ArgTypes, Markdown } from '@storybook/addon-docs/blocks';
 
-// @ts-expect-error Untyped import. See `decisions/imports-ts-suppressions.md`.
-import { BpkDarkExampleWrapper } from 'bpk-storybook-utils';
 
 import { BpkFlareBar, BpkContentBubble } from "..";
 import BpkText, { TEXT_STYLES } from '../../bpk-component-text';
@@ -153,19 +151,6 @@ const MixedExample = () => (
   </div>
 );
 
-const DarkModeExample = () => (
-  <BpkDarkExampleWrapper>
-    <div className={getClassName('bpk-flare-stories__container')}>
-      <BpkContentBubble
-        className={getClassName('bpk-flare-stories__content-bubble')}
-        rounded
-        flareProps={{svgClassName: getClassName('bpk-flare-stories__flare-bar--svg')}}
-        content={contentShort}
-      />
-    </div>
-  </BpkDarkExampleWrapper>
-);
-
 export default {
   title: 'bpk-component-flare',
   component: BpkContentBubble,
@@ -210,10 +195,6 @@ export const BpkContentBubbleFixedHeight = {
 
 export const BpkContentBubblePointerHiddenRounded = {
   render: () => <ContentBubblePointerHiddenRoundedExample />,
-};
-
-export const BpkContentBubbleDarkMode = {
-  render: () => <DarkModeExample />,
 };
 
 export const VisualTest = {
