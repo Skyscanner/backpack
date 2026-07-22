9075383cbd04ab9ea537cdc9d0079ce4581562df
Gert-Jan Vercauteren <gert-jan.vercauteren@skyscanner.net>
2026-07-22 18:46:07 +0800

    chore: reset non-CSS files to main

--- packages/backpack-web/src/bpk-component-popover/src/BpkPopover.stories.js
+++ packages/backpack-web/src/bpk-component-popover/src/BpkPopover.stories.js
@@ -21,7 +21,6 @@ import { Component, createRef } from 'react';
 
 import { ArgTypes, Markdown } from '@storybook/addon-docs/blocks';
 
-import { BpkDarkExampleWrapper } from 'bpk-storybook-utils';
 
 import BpkButton from '../../bpk-component-button';
 import BpkInput, { withOpenEvents } from '../../bpk-component-input';
@@ -223,13 +222,3 @@ export const TriggeredByInput = { render: () => <InputTriggerExample /> };
 export const WithActionButton = { render: () => <WithActionButtonExample /> };
 export const VisualTest = { render: () => <VisualExample /> };
 export const VisualTestWithZoom = { render: () => <VisualExample />, args: { zoomEnabled: true } };
-
-const DarkExample = () => (
-  <BpkDarkExampleWrapper>
-    <Spacer>
-      <PopoverContainer id="my-popover-dark" isOpen />
-    </Spacer>
-  </BpkDarkExampleWrapper>
-);
-
-export const VisualTestDark = { render: () => <DarkExample /> };
