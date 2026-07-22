79ebac67dc86762e640b3fedddd79e7ac701e0a7
Gert-Jan Vercauteren <gert-jan.vercauteren@skyscanner.net>
2026-07-22 16:43:35 +0800

    [CLOV-CSS] Migrate bpk-component-autosuggest to CSS custom properties

---
+++ packages/backpack-web/src/bpk-component-autosuggest/src/themeAttributes.tsx
@@ -0,0 +1,31 @@
+/*
+ * Backpack - Skyscanner's Design System
+ *
+ * Copyright 2016 Skyscanner Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * Combined deduplicated array of all autosuggest private theme attributes.
+ * Only exposes component-scoped (private) autosuggest tokens — global semantic vars
+ * (text, surface, canvas colours) can be overridden at the theme level independently.
+ */
+export const autosuggestThemeAttributes: string[] = [
+  'privateAutosuggestColourBgDefault',
+  'privateAutosuggestColourBgHover',
+  'privateAutosuggestColourBorderDefault',
+  'privateAutosuggestColourTextClose',
+];
+
+export default autosuggestThemeAttributes;
