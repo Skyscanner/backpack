7dda63bba0b21e4e4f97fdbc35c1c80bdb943ac1
Gert-Jan Vercauteren <gert-jan.vercauteren@skyscanner.net>
2026-07-22 18:48:44 +0800

    chore: reset non-CSS files to main

--- packages/backpack-web/src/bpk-component-overlay/src/BpkOverlay.stories.tsx
+++ packages/backpack-web/src/bpk-component-overlay/src/BpkOverlay.stories.tsx
@@ -18,9 +18,6 @@
 
 import { ArgTypes, Markdown } from '@storybook/addon-docs/blocks';
 
-// @ts-expect-error Untyped import
-import { BpkDarkExampleWrapper } from 'bpk-storybook-utils';
-
 import BpkImage from '../../bpk-component-image';
 import BpkText, { TEXT_STYLES } from '../../bpk-component-text';
 import { cssModules } from '../../bpk-react-utils';
@@ -335,12 +332,4 @@ export const VisualTestWithZoom = {
   args: {
     zoomEnabled: true,
   },
-};
-
-export const DarkMode = {
-  render: () => (
-    <BpkDarkExampleWrapper>
-      <MixedExample />
-    </BpkDarkExampleWrapper>
-  ),
 };
